bd9aec555ae82610917a25cf94c4afc4f6f93f55
Czhipu <czhipu@126.com>
2023-02-13 14:55:19 +0800

    feat: 添加备件注册接口（待完善）

---
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Equipment/EquSparePart/Query/EquSparePartPagedQuery.cs
@@ -0,0 +1,12 @@
+using Hymson.Infrastructure;
+
+namespace Hymson.MES.Data.Repositories.Equipment.EquSparePart.Query
+{
+    /// <summary>
+    /// 备件注册 分页参数
+    /// </summary>
+    public class EquSparePartPagedQuery : PagerInfo
+    {
+
+    }
+}
---
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Equipment/EquSparePart/Query/EquSparePartQuery.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymson.MES.Data.Repositories.Equipment.EquSparePart.Query
+{
+    /// <summary>
+    /// 备件注册 查询参数
+    /// </summary>
+    public class EquSparePartQuery
+    {
+    }
+}
