3cbb3e297675c7d556d7a7d050fb6886c6323513
longkai <longkai@hymson.com>
2023-02-24 14:44:58 +0800

    feat: 产品参数 ok

--- HymsonMES/src/Libraries/Hymson.MES.Services/Dtos/Process/ProcParameterLinkTypeDto.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Dtos/Process/ProcParameterLinkTypeDto.cs
@@ -162,22 +162,22 @@ namespace Hymson.MES.Services.Dtos.Process
         /// <summary>
         /// 创建人
         /// </summary>
-        public string CreateBy { get; set; }
+        public string CreatedBy { get; set; }
 
         /// <summary>
         /// 创建时间
         /// </summary>
-        public DateTime CreateOn { get; set; }
+        public DateTime CreatedOn { get; set; }
 
         /// <summary>
         /// 更新人
         /// </summary>
-        public string? UpdateBy { get; set; }
+        public string? UpdatedBy { get; set; }
 
         /// <summary>
         /// 更新时间
         /// </summary>
-        public DateTime? UpdateOn { get; set; }
+        public DateTime? UpdatedOn { get; set; }
 
         /// <summary>
         /// 备注
