3add926febaa118241e235bc9c89ae1f3e42bb84
pengxin <dell@ANKY>
2023-03-03 18:37:10 +0800

    feat:创建库存/供应商管理

---
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Warehouse/WhSupplierQuery.cs
@@ -0,0 +1,23 @@
+/*
+ *creator: Karl
+ *
+ *describe: 供应商 查询类 | 代码由框架生成
+ *builder:  pengxin
+ *build datetime: 2023-03-03 01:51:43
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymson.MES.Data.Repositories.Warehouse
+{
+    /// <summary>
+    /// 供应商 查询参数
+    /// </summary>
+    public class WhSupplierQuery
+    {
+    }
+}
---
+++ HymsonMES/src/Presentation/Hymson.MES.Api/Controllers/Warehouse/WhSupplierController.cs
@@ -0,0 +1,105 @@
+/*
+ *creator: Karl
+ *
+ *describe: 供应商    控制器 | 代码由框架生成  
+ *builder:  pengxin
+ *build datetime: 2023-03-03 01:51:43
+ */
+using Hymson.Infrastructure;
+using Hymson.MES.Services.Dtos.Warehouse;
+using Hymson.MES.Services.Services.Warehouse;
+//using Hymson.Utils.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Extensions;
+
+namespace Hymson.MES.Api.Controllers.Warehouse
+{
+    /// <summary>
+    /// 控制器（供应商）
+    /// @author pengxin
+    /// @date 2023-03-03 01:51:43
+    /// </summary>
+    [Authorize]
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class WhSupplierController : ControllerBase
+    {
+        /// <summary>
+        /// 接口（供应商）
+        /// </summary>
+        private readonly IWhSupplierService _whSupplierService;
+        private readonly ILogger<WhSupplierController> _logger;
+
+        /// <summary>
+        /// 构造函数（供应商）
+        /// </summary>
+        /// <param name="whSupplierService"></param>
+        public WhSupplierController(IWhSupplierService whSupplierService, ILogger<WhSupplierController> logger)
+        {
+            _whSupplierService = whSupplierService;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// 分页查询列表（供应商）
+        /// </summary>
+        /// <param name="parm"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("pagelist")]
+        public async Task<PagedInfo<WhSupplierDto>> QueryPagedWhSupplierAsync([FromQuery] WhSupplierPagedQueryDto parm)
+        {
+            return await _whSupplierService.GetPageListAsync(parm);
+        }
+
+        /// <summary>
+        /// 查询详情（供应商）
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<WhSupplierDto> QueryWhSupplierByIdAsync(long id)
+        {
+            return await _whSupplierService.QueryWhSupplierByIdAsync(id);
+        }
+
+        /// <summary>
+        /// 添加（供应商）
+        /// </summary>
+        /// <param name="parm"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("create")]
+        public async Task AddWhSupplierAsync([FromBody] WhSupplierCreateDto parm)
+        {
+             await _whSupplierService.CreateWhSupplierAsync(parm);
+        }
+
+        /// <summary>
+        /// 更新（供应商）
+        /// </summary>
+        /// <param name="parm"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("update")]
+        public async Task UpdateWhSupplierAsync([FromBody] WhSupplierModifyDto parm)
+        {
+             await _whSupplierService.ModifyWhSupplierAsync(parm);
+        }
+
+        /// <summary>
+        /// 删除（供应商）
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("delete")]
+        public async Task DeleteWhSupplierAsync(string ids)
+        {
+            //long[] idsArr = StringExtension.SpitLongArrary(ids);
+            await _whSupplierService.DeletesWhSupplierAsync(ids);
+        }
+
+    }
+}
