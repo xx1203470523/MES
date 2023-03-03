74b821f18c8683d87c57d0edbef2fe67af194680
zhao.qing <zhaoqing@hymson.com>
2023-03-03 18:01:59 +0800

    feat: 状态枚举添加中文注释

--- HymsonMES/src/Libraries/Hymson.MES.Core/Enums/SysDataStatusEnum.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Core/Enums/SysDataStatusEnum.cs
@@ -1,28 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hymson.MES.Core.Enums
 {
-    public enum SysDataStatusEnum:short
+    public enum SysDataStatusEnum : short
     {
         /// <summary>
         /// 新建
         /// </summary>
+        [Description("新建")]
         Build = 0,
         /// <summary>
         /// 启用
         /// </summary>
+        [Description("启用")]
         Enable = 1,
         /// <summary>
         /// 保留
         /// </summary>
+        [Description("保留")]
         Retain = 2,
         /// <summary>
         /// 废除
         /// </summary>
+        [Description("废除")]
         Abolish = 3,
     }
 }
