51a20c8283dac07f471b37dee5488373efdcb45d
longkai <longkai@hymson.com>
2023-03-14 11:10:12 +0800

    feat: fixbug

--- HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
@@ -65,7 +65,7 @@
         public const string MES10402 = "工序编码超长";
         public const string MES10403 = "工序名称不能为空";
         public const string MES10404 = "工序名称超长";
-        public const string MES10405 = "编码:{0}已存在！";
+        public const string MES10405 = "编码:{Code}已存在！";
 
         public const string MES10430 = $"不能删除启用和保留状态的工艺路线！";
         public const string MES10431 = $"此工艺路线在系统中已经存在!";
