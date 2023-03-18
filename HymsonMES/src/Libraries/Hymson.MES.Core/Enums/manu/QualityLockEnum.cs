c573d8812c906dc6af5fb7b4cbe7ffcfb3ff857a
zhao.qing <zhaoqing@hymson.com>
2023-03-18 17:43:33 +0800

    feat: 参加枚举和实体

---
+++ HymsonMES/src/Libraries/Hymson.MES.Core/Enums/manu/QualityLockEnum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymson.MES.Core.Enums
+{
+    /// <summary>
+    /// 质量锁定操作状态
+    /// </summary>
+    public enum QualityLockEnum : sbyte
+    {
+        /// <summary>
+        /// 即时锁
+        /// </summary>
+        [Description("即时锁")]
+        InstantLock =2,
+        /// <summary>
+        /// 将来锁
+        /// </summary>
+        [Description("将来锁")]
+        FutureLock = 3,
+        /// <summary>
+        /// 取消锁定
+        /// </summary>
+        [Description("取消锁定")]
+        Unlock = 1
+    }
+}
---
+++ HymsonMES/src/Libraries/Hymson.MES.Core/Enums/manu/SfcStatusEnum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymson.MES.Core.Enums.manu
+{
+    /// <summary>
+    /// 条码状态枚举
+    /// </summary>
+    public enum SfcStatusEnum : sbyte
+    {
+        /// <summary>
+        /// 在制
+        /// </summary>
+        [Description("在制")]
+        InProcess = 1,
+        /// <summary>
+        /// 完成
+        /// </summary>
+        [Description("完成")]
+        Complete = 2,
+        /// <summary>
+        /// 已入库
+        /// </summary>
+        [Description("已入库")]
+        Received = 3,
+        /// <summary>
+        /// 报废
+        /// </summary>
+        [Description("报废")]
+        Scrapping = 4
+    }
+}
