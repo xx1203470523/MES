e3563652ab189cfb9f1e66ccb6148e2961ebe173
zhao.qing <zhaoqing@hymson.com>
2023-03-30 09:16:54 +0800

    feat: 不良录入

--- HymsonMES/src/Libraries/Hymson.MES.Services/Services/Manufacture/ManuProductBadRecord/ManuProductBadRecordService.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Services/Manufacture/ManuProductBadRecord/ManuProductBadRecordService.cs
@@ -105,7 +105,11 @@ namespace Hymson.MES.Services.Services.Manufacture
             var qualUnqualifiedCodes = await _qualUnqualifiedCodeRepository.GetByIdsAsync(createDto.UnqualifiedIds);
 
             var manuProductBadRecords = new List<ManuProductBadRecordEntity>();
-
+            long badResourceId = 0;
+            if (!string.IsNullOrWhiteSpace(createDto.FoundBadResourceId))
+            {
+                badResourceId = createDto.FoundBadResourceId.ParseToLong();
+            }
             foreach (var item in manuSfcs)
             {
                 foreach (var unqualified in qualUnqualifiedCodes)
@@ -115,7 +119,7 @@ namespace Hymson.MES.Services.Services.Manufacture
                         Id = IdGenProvider.Instance.CreateId(),
                         SiteId = _currentSite.SiteId ?? 0,
                         FoundBadOperationId = createDto.FoundBadOperationId,
-                        FoundBadResourceId= createDto.FoundBadResourceId,
+                        FoundBadResourceId= badResourceId,
                         OutflowOperationId = createDto.OutflowOperationId,
                         UnqualifiedId = unqualified.Id,
                         SFC = item.SFC,
