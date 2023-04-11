61f622a8289c568d323d3bda0b70126d07db4ca0
longkai <longkai@hymson.com>
2023-04-11 11:10:59 +0800

    feat: 获取面板byCode

--- HymsonMES/src/Presentation/Hymson.MES.Api/Controllers/Manufacture/ManuFacePlateController.cs
+++ HymsonMES/src/Presentation/Hymson.MES.Api/Controllers/Manufacture/ManuFacePlateController.cs
@@ -109,7 +109,7 @@ namespace Hymson.MES.Api.Controllers.Manufacture
         /// </summary>
         /// <param name="code"></param>
         /// <returns></returns>
-        [HttpGet("{code}")]
+        [HttpGet("QueryByCode/{code}")]
         public async Task<QueryManuFacePlateDto> QueryManuFacePlateByCodeAsync(string code)
         {
             return await _manuFacePlateService.QueryManuFacePlateByCodeAsync(code);
