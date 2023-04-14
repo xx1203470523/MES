995ff55460b6a97c453f1c45ada7e371b696d56c
zhao.qing <zhaoqing@hymson.com>
2023-04-14 16:30:17 +0800

    feat: 在制品拆解添加

--- HymsonMES/src/Libraries/Hymson.MES.Services/Services/Manufacture/InProductDismantle/IInProductDismantleService.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Services/Manufacture/InProductDismantle/IInProductDismantleService.cs
@@ -12,8 +12,29 @@ namespace Hymson.MES.Services.Services.Manufacture
         /// <summary>
         /// 根据ID查询Bom 详情
         /// </summary>
-        /// <param name="bomId"></param>
+        /// <param name="queryDto"></param>
         /// <returns></returns>
         Task<List<InProductDismantleDto>> GetProcBomDetailAsync(InProductDismantleQueryDto queryDto);
+
+        /// <summary>
+        /// 在制品移除
+        /// </summary>
+        /// <param name="removeDto"></param>
+        /// <returns></returns>
+        Task RemoveModuleAsync(InProductDismantleRemoveDto removeDto);
+
+        /// <summary>
+        /// 在制品拆解添加组件
+        /// </summary>
+        /// <param name="addDto"></param>
+        /// <returns></returns>
+        Task AddModuleAsync(InProductDismantleAddDto addDto);
+
+        /// <summary>
+        /// 在制品拆解换件
+        /// </summary>
+        /// <param name="replaceDto"></param>
+        /// <returns></returns>
+        Task ReplaceModuleAsync(InProductDismantleReplaceDto replaceDto);
     }
 }
