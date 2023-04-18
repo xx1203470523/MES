c35dc216eee914cbfaf66a341b8cd9ee48019a7b
JinYi <JinYi@hymson.com>
2023-04-18 15:20:47 +0800

    添加httpclient 层

---
+++ HymsonMES/src/Libraries/Hymson.MES.HttpClients/DependencyInjection/HttpClientCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class HttpClientCollectionExtensions
+    {
+        /// <summary>
+        /// 业务逻辑层依赖服务添加
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddHttpClientService(this IServiceCollection services, IConfiguration configuration)
+        {
+           
+            return services;
+        }
+
+        
+
+        /// <summary>
+        /// 添加配置
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        private static IServiceCollection AddConfig(IServiceCollection services, IConfiguration configuration)
+        {
+            //数据库连接
+            //services.Configure<TestOptions>(configuration.GetSection(nameof(TestOptions)));
+            //services.Configure<ConnectionOptions>(configuration);
+            return services;
+        }
+
+        
+    }
+}
