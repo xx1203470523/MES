98d15fd8e84f5c9ce393629107df22fe464e0066
longkai <longkai@hymson.com>
2023-04-21 17:48:15 +0800

    feat: 不良记录报表

---
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Dtos/Report/BadRecordReportDto.cs
@@ -0,0 +1,79 @@
+﻿using Hymson.Infrastructure;
+using Hymson.MES.Core.Enums;
+using Hymson.MES.Core.Enums.Integrated;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymson.MES.Services.Dtos.Report
+{
+    public class BadRecordReportDto : PagerInfo
+    {
+        ///// <summary>
+        ///// 站点id
+        ///// </summary>
+        //public long SiteId { get; set; }
+
+        /// <summary>
+        /// 物料编码
+        /// </summary>
+        public string? MaterialCode { get; set; }
+
+        /// <summary>
+        /// 物料版本
+        /// </summary>
+        public string? MaterialVersion { get; set; }
+
+        /// <summary>
+        /// 工单编码
+        /// </summary>
+        public string? OrderCode { get; set; }
+
+        /// <summary>
+        /// 条码
+        /// </summary>
+        public string? SFC { get; set; }
+
+        /// <summary>
+        /// 工序编码
+        /// </summary>
+        public string? ProcedureCode { get; set; }
+
+        /// <summary>
+        /// 创建时间-开始
+        /// </summary>
+        public DateTime? CreatedOnS { get; set; }
+
+        /// <summary>
+        /// 创建时间-结束
+        /// </summary>
+        public DateTime? CreatedOnE { get; set; }
+    }
+
+    public class ManuProductBadRecordReportViewDto
+    {
+        /// <summary>
+        /// 不合格代码Id
+        /// </summary>
+        public long UnqualifiedId { get; set; }
+
+        /// <summary>
+        /// 汇总数量
+        /// </summary>
+        public decimal Num { get; set; }
+
+        /// <summary>
+        /// 描述 :不合格代码 
+        /// 空值 : false  
+        /// </summary>
+        public string UnqualifiedCode { get; set; }
+
+        /// <summary>
+        /// 描述 :不合格代码名称 
+        /// 空值 : false  
+        /// </summary>
+        public string UnqualifiedCodeName { get; set; }
+    }
+}
---
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Services/Report/BadRecord/BadRecordReportService.cs
@@ -0,0 +1,88 @@
+using FluentValidation;
+using Hymson.Authentication;
+using Hymson.Authentication.JwtBearer.Security;
+using Hymson.Infrastructure;
+using Hymson.Infrastructure.Exceptions;
+using Hymson.Infrastructure.Mapper;
+using Hymson.MES.Core.Constants;
+using Hymson.MES.Core.Domain.Plan;
+using Hymson.MES.Core.Enums;
+using Hymson.MES.Data.Repositories.Common.Command;
+using Hymson.MES.Data.Repositories.Integrated.IIntegratedRepository;
+using Hymson.MES.Data.Repositories.Manufacture;
+using Hymson.MES.Data.Repositories.Plan;
+using Hymson.MES.Data.Repositories.Plan.PlanWorkOrder.Query;
+using Hymson.MES.Data.Repositories.Process;
+using Hymson.MES.Data.Repositories.Quality;
+using Hymson.MES.Data.Repositories.Quality.IQualityRepository;
+using Hymson.MES.Services.Dtos.Plan;
+using Hymson.MES.Services.Dtos.Report;
+using Hymson.MES.Services.Services.Report;
+using Hymson.Snowflake;
+using Hymson.Utils;
+using Hymson.Utils.Tools;
+using System.Transactions;
+
+namespace Hymson.MES.Services.Services.Report
+{
+    /// <summary>
+    /// 不良记录报表 服务
+    /// </summary>
+    public class BadRecordReportService : IBadRecordReportService
+    {
+        private readonly ICurrentUser _currentUser;
+        private readonly ICurrentSite _currentSite;
+
+        /// <summary>
+        /// 工单信息表 仓储
+        /// </summary>
+        private readonly IManuProductBadRecordRepository _manuProductBadRecordRepository;
+        private readonly IQualUnqualifiedCodeRepository _qualUnqualifiedCodeRepository;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="currentUser"></param>
+        /// <param name="currentSite"></param>
+        public BadRecordReportService(ICurrentUser currentUser, ICurrentSite currentSite, IManuProductBadRecordRepository manuProductBadRecordRepository, IQualUnqualifiedCodeRepository qualUnqualifiedCodeRepository)
+        {
+            _currentUser = currentUser;
+            _currentSite = currentSite;
+
+            _manuProductBadRecordRepository=manuProductBadRecordRepository;
+            _qualUnqualifiedCodeRepository = qualUnqualifiedCodeRepository;
+        }
+
+        /// <summary>
+        /// 根据查询条件获取不良报表分页数据
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public async Task<PagedInfo<ManuProductBadRecordReportViewDto>> GetPageListAsync(BadRecordReportDto param)
+        {
+            var pagedQuery = param.ToQuery<ManuProductBadRecordReportPagedQuery>();
+            pagedQuery.SiteId = _currentSite.SiteId;
+            var pagedInfo = await _manuProductBadRecordRepository.GetPagedInfoReportAsync(pagedQuery);
+
+            var unqualifiedIds= pagedInfo.Data.Select(x=>x.UnqualifiedId).ToArray();
+            var unqualifiedCodeEntities = await _qualUnqualifiedCodeRepository.GetByIdsAsync(unqualifiedIds);
+
+            List< ManuProductBadRecordReportViewDto > listDto=new List< ManuProductBadRecordReportViewDto >();
+            foreach (var item in pagedInfo.Data)
+            {
+                var unqualifiedCodeEntitie = unqualifiedCodeEntities.Where(y => y.Id == item.UnqualifiedId).FirstOrDefault();
+
+                listDto.Add(new ManuProductBadRecordReportViewDto
+                {
+                    UnqualifiedId = item.UnqualifiedId,
+                    Num = item.Num,
+                    UnqualifiedCode = unqualifiedCodeEntitie?.UnqualifiedCode ?? "",
+                    UnqualifiedCodeName = unqualifiedCodeEntitie?.UnqualifiedCodeName??""
+                });
+            }
+            
+            return new PagedInfo<ManuProductBadRecordReportViewDto>(listDto, pagedInfo.PageIndex, pagedInfo.PageSize, pagedInfo.TotalCount);
+        }
+
+    }
+}
