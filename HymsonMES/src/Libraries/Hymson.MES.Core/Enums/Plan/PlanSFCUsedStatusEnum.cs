b3db5e7d9fa0fbc6854effb8c0f8223d84403dd1
Czhipu <czhipu@126.com>
2023-05-06 09:25:27 +0800

    feat: bugfix

--- HymsonMES/src/Libraries/Hymson.MES.Core/Enums/Plan/PlanSFCUsedStatusEnum.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Core/Enums/Plan/PlanSFCUsedStatusEnum.cs
@@ -3,19 +3,19 @@
 namespace Hymson.MES.Core.Enums
 {
     /// <summary>
-    /// 条码使用状态（这个类型前端有用到）
+    /// 条码使用状态（这个类型前端有用到，对应 YesOrNoEnum）
     /// </summary>
     public enum PlanSFCUsedStatusEnum : sbyte
     {
         /// <summary>
-        /// 未使用 
-        /// </summary> 
-        [Description("未使用")]
-        NotUsed = 1,
-        /// <summary>
         /// 已使用
         /// </summary>
         [Description("已使用")]
-        Used = 2,
+        Used = 1,
+        /// <summary>
+        /// 未使用 
+        /// </summary> 
+        [Description("未使用")]
+        NotUsed = 2
     }
 }
