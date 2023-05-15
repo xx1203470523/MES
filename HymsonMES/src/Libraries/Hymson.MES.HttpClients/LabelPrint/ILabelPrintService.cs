4634091d2310ee3ca351a5921ceef030d60615db
wxk <wangxuke@hymson.com>
2023-05-15 20:36:14 +0800

    feat: 标签打印服务

---
+++ HymsonMES/src/Libraries/Hymson.MES.HttpClients/LabelPrint/ILabelPrintService.cs
@@ -0,0 +1,25 @@
+﻿
+using Hymson.MES.HttpClients.Requests.Print;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymson.MES.HttpClients
+{
+    public interface ILabelPrintService
+    {
+        /// <summary>
+        /// 打印预览图片
+        /// </summary>
+        /// <returns></returns>
+        public Task<(string base64Str, bool result)> PreviewFromImageBase64Async(PrintRequest  printRequest);
+        /// <summary>
+        ///标签打印
+        /// </summary>
+        /// <param name="ShowDialog"></param>
+        /// <returns></returns>
+        public Task<(string msg, bool result)> PrintAsync(PrintRequest printRequest, bool ShowDialog = false);
+    }
+}
---
+++ HymsonMES/src/Libraries/Hymson.MES.HttpClients/Requests/Print/PrintRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymson.MES.HttpClients.Requests.Print
+{
+    public class PrintRequest
+    {
+        /// <summary>
+        /// 模板名称
+        /// </summary>
+        public string TemplateName { get; set; }
+        /// <summary>
+        /// 打印机名称
+        /// </summary>
+        public string PrinterName { get; set; }
+        /// <summary>
+        /// 传递给报表的参数集合
+        /// </summary>
+        public List<ParamEntity> Params { get; set; }
+        /// <summary>
+        /// 入参实体
+        /// </summary>
+        public class ParamEntity
+        {
+            /// <summary>
+            /// 入参变量名称
+            /// </summary>
+            public string ParamName { get; set; }
+            /// <summary>
+            /// 入参变量值内容
+            /// </summary>
+            public string ParamValue { get; set; }
+
+        }
+    }
+}
