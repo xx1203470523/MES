d0ec67536815ac2bf5f8d5b39cf44bb821fcc62e
wangkeming <wangkeming@hymson.com>
2023-05-15 14:43:12 +0800

    fix: 修改查询

---
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Integrated/InteCodeRule/Query/InteCodeRulesByProductQuery.cs
@@ -0,0 +1,20 @@
+﻿using Hymson.MES.Core.Enums.Integrated;
+
+namespace Hymson.MES.Data.Repositories.Integrated.InteCodeRule.Query
+{
+    /// <summary>
+    /// 查询产品编码规则
+    /// </summary>
+    public class InteCodeRulesByProductQuery
+    {
+        /// <summary>
+        /// 产品id  物料
+        /// </summary>
+        public long ProductId { get; set; }
+
+        /// <summary>
+        /// 编码类型;1：过程控制序列码；2：包装序列码；
+        /// </summary>
+        public CodeRuleCodeTypeEnum? CodeType { get; set; }
+    }
+}
