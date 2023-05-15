10fa0a6416d6220dc81393d44e80b2ff57d6e3c5
Czhipu <czhipu@126.com>
2023-05-15 18:43:54 +0800

    feat: bugfix

--- HymsonMES/src/Libraries/Hymson.MES.Services/Services/Manufacture/ManuMainstreamProcess/ManuCommon/ManuCommonService.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Services/Manufacture/ManuMainstreamProcess/ManuCommon/ManuCommonService.cs
@@ -844,6 +844,9 @@ namespace Hymson.MES.Services.Services.Manufacture.ManuMainstreamProcess.ManuCom
         /// <param name="produceStatus"></param>
         public static ManuSfcProduceEntity VerifySFCStatus(this ManuSfcProduceEntity sfcProduceEntity, SfcProduceStatusEnum produceStatus)
         {
+            // 当前工序是否是被锁定
+            if (sfcProduceEntity.Status == SfcProduceStatusEnum.Locked) throw new CustomerValidationException(nameof(ErrorCode.MES16314)).WithData("SFC", sfcProduceEntity.SFC);
+
             // 当前工序是否是指定状态
             if (sfcProduceEntity.Status != produceStatus) throw new CustomerValidationException(nameof(ErrorCode.MES16313)).WithData("Status", produceStatus.GetDescription());
 
