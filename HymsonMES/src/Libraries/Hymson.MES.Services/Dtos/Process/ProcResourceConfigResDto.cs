a4b26072d8a0018d2a0b3b2f7941cc51f031cdef
zhao.qing <zhaoqing@hymson.com>
2023-05-22 18:51:49 +0800

    feat: 资源设置，工单、物料设置值展示

--- HymsonMES/src/Libraries/Hymson.MES.Services/Dtos/Process/ProcResourceConfigResDto.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Dtos/Process/ProcResourceConfigResDto.cs
@@ -41,9 +41,19 @@ namespace Hymson.MES.Services.Dtos.Process
         /// </summary>
         public string Value { get; set; }
 
-       /// <summary>
-        /// 备注
+        /// <summary>
+        /// 工单号
         /// </summary>
+        public string OrderCode { get; set; }
+
+        /// <summary>
+        /// 物料编码
+        /// </summary>
+        public string MaterialCode { get; set; }
+
+       /// <summary>
+       /// 备注
+       /// </summary>
         public string Remark { get; set; }
 
        /// <summary>
