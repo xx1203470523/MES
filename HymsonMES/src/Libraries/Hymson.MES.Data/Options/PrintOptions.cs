c0d893dfcb9071b8c64f8452087357f6ee6cb19d
wxk <wangxuke@hymson.com>
2023-05-23 17:01:47 +0800

    feat: 打印添加options 配置项

---
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Options/PrintOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymson.MES.Data.Options
+{
+    public class PrintOptions
+    {
+        public string BaseAddressUri { get; set; }
+    }
+}
