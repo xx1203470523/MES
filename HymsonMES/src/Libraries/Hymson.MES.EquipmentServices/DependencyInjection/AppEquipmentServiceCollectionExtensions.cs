080810e2824c008da338be81b5d89579fb945b11
pengxin <dell@ANKY>
2023-06-06 12:25:25 +0800

    feat:通用Job注入

--- HymsonMES/src/Libraries/Hymson.MES.EquipmentServices/DependencyInjection/AppEquipmentServiceCollectionExtensions.cs
+++ HymsonMES/src/Libraries/Hymson.MES.EquipmentServices/DependencyInjection/AppEquipmentServiceCollectionExtensions.cs
@@ -38,6 +38,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddSingleton<IInStationService, InStationService>();
             services.AddSingleton<IJobManufactureService, JobManuSfcConvertService>();
             services.AddSingleton<ISfcBindingService, SfcBindingService>();
+            services.AddSingleton<IJobCommonService, JobCommonService>();
 
         }
 
