9bea9d8b509904d5de72a4bb897620eab8f1d474
Czhipu <czhipu@126.com>
2023-06-06 15:14:39 +0800

    feat: bugfix 2081

--- HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
@@ -611,6 +611,7 @@
         public const string MES16504 = "条码已经存在。";
         public const string MES16505 = "条码不存在，无法复用。";
         public const string MES16506 = "条码不为完成和在库状态，无法复用。";
+        public const string MES16507 = "资源和工序对应的资源类型不匹配。";
         #endregion
 
         #region 在制品移除添加 16600
