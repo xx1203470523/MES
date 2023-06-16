e43983544a4944302a141a6118a963e436ae3653
longkai <longkai@hymson.com>
2023-06-16 10:02:50 +0800

    feat: 2243 中英文切换---删除启用状态下的容器维护项，弹出的报错信息有误

--- HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
@@ -288,7 +288,7 @@
         public const string MES12507 = "最大数量须大于0";
         public const string MES12508 = "最大数量须大于最小数量";
         public const string MES12509 = "只能删除新建状态的数据";
-        public const string MES12510 = "只能删除新建状态的数据";
+        public const string MES12510 = "非新建状态的数据不能修改为新建状态";
         #endregion
 
         #region 设备 12600
