02a762950396e4f94b5e3cf611febfd5ac441a32
pengxin <pengxin@163.com>
2023-06-16 14:22:06 +0800

    feat: bug修复 2271 接口测试-工作中心维护-类型type可填写非2，3数字也能成功

--- HymsonMES/src/Libraries/Hymson.MES.Services/Validators/Integrated/InteWorkCenterValidator.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Validators/Integrated/InteWorkCenterValidator.cs
@@ -46,6 +46,9 @@ namespace Hymson.MES.Services.Validators.Integrated
             // RuleFor(x => x.Source).NotEmpty().WithErrorCode(nameof(ErrorCode.MES12107));
             // RuleFor(x => x.IsMixLine).NotEmpty().WithErrorCode(nameof(ErrorCode.MES12108));
             RuleFor(x => x.Remark).MaximumLength(255).WithErrorCode(nameof(ErrorCode.MES12109));
+            RuleFor(x => x.Status).NotEmpty().WithErrorCode(nameof(ErrorCode.MES12122));
+            RuleFor(x => x).Must(it => Enum.IsDefined(typeof(SysDataStatusEnum), it.Status ?? 0)).WithErrorCode(nameof(ErrorCode.MES12123));
+            RuleFor(x => x).Must(it => Enum.IsDefined(typeof(WorkCenterTypeEnum), it.Type ?? 0)).WithErrorCode(nameof(ErrorCode.MES12124));
         }
     }
 }
