914b1da45ca8d7763a6e9d05e5a1413ea4e68b6c
pengxin <pengxin@163.com>
2023-06-19 16:30:42 +0800

    feat: bug修复 2303 接口测试-标准参数-parameterUnit可填写非1，2，3如4，页面不显示但是数据能入库；另一个问题是数据库数据比页面显示的多，页面删除的数据库仍存在

--- HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
@@ -149,6 +149,7 @@
         public const string MES10509 = "参数编码不能为空";
         public const string MES10510 = "参数名称不能为空";
         public const string MES10511 = "标准参数代码最大长度为50";
+        public const string MES10512 = "参数单位不合法，请检查！"; 
 
         #endregion
 
