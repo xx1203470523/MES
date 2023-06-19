c2d2a70cb880278860c36761c5474a677455164a
pengxin <pengxin@163.com>
2023-06-19 15:56:24 +0800

    feat: bug修复 2282 接口测试-容器维护-包装等级可以填写非1，2，3；填写0则显示0，填写4仍为一级但数据显示4

--- HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
@@ -320,7 +320,8 @@
         public const string MES12508 = "最大数量须大于最小数量";
         public const string MES12509 = "只能删除新建状态的数据";
         public const string MES12510 = "非新建状态的数据不能修改为新建状态";
-        public const string MES12511 = "状态不合法，请检查";
+        public const string MES12511 = "状态不合法，请检查"; 
+        public const string MES12512 = "保证等级不合法，请检查";
         #endregion
 
         #region 设备 12600
