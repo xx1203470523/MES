3209ba13a25b7cbfabf57fd5ddfc6a9dee1c4c04
pengxin <pengxin@163.com>
2023-06-27 15:41:55 +0800

    feat：Job结构创建

---
+++ HymsonMES/src/Libraries/Hymson.MES.CoreServices/Services/Job/SfcConvertService.cs
@@ -0,0 +1,45 @@
+﻿using Hymson.MES.Core.Attribute.Job;
+using Hymson.MES.Core.Enums.Job;
+using Hymson.MES.CoreServices.Bos.Job;
+using Hymson.MES.CoreServices.Services.Job;
+
+namespace Hymson.MES.CoreServices.Services.NewJob
+{
+    /// <summary>
+    /// 条码转换
+    /// </summary>
+    [Job("条码转换", JobTypeEnum.Standard)]
+    public class SfcConvertService : IJobService<SfcConvertRequestBo, SfcConvertResponseBo>
+    {
+        /// <summary>
+        /// 参数校验
+        /// </summary> 
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public async Task VerifyParamAsync(SfcConvertRequestBo param)
+        {
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 数据组装
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public async Task<SfcConvertResponseBo> DataAssemblingAsync(SfcConvertRequestBo param)
+        {
+            await Task.CompletedTask;
+            return new SfcConvertResponseBo { };
+        }
+
+        /// <summary>
+        /// 执行入库
+        /// </summary>
+        /// <returns></returns>
+        public async Task ExecuteAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+    }
+}
