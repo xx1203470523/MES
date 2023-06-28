f1b624e71a9821b3bf002b79d85830183548419d
Czhipu <czhipu@126.com>
2023-06-28 18:06:53 +0800

    feat: add InStationVerifyJobService

--- HymsonMES/src/Libraries/Hymson.MES.CoreServices/Services/Job/InStationJobService.cs
+++ HymsonMES/src/Libraries/Hymson.MES.CoreServices/Services/Job/InStationJobService.cs
@@ -1,13 +1,8 @@
-﻿using Hymson.Infrastructure.Exceptions;
-using Hymson.MES.Core.Attribute.Job;
-using Hymson.MES.Core.Constants;
-using Hymson.MES.Core.Enums;
+﻿using Hymson.MES.Core.Attribute.Job;
 using Hymson.MES.Core.Enums.Job;
 using Hymson.MES.CoreServices.Bos.Job;
 using Hymson.MES.CoreServices.Services.Common.ManuCommon;
-using Hymson.MES.CoreServices.Services.Common.ManuExtension;
 using Hymson.MES.CoreServices.Services.Job;
-using Hymson.MES.Data.Repositories.Process;
 
 namespace Hymson.MES.CoreServices.Services.NewJob
 {
@@ -22,29 +17,14 @@ namespace Hymson.MES.CoreServices.Services.NewJob
         /// </summary>
         private readonly IManuCommonService _manuCommonService;
 
-        /// <summary>
-        /// 仓储接口（工艺路线工序节点）
-        /// </summary>
-        private readonly IProcProcessRouteDetailNodeRepository _procProcessRouteDetailNodeRepository;
-
-        /// <summary>
-        /// 仓储接口（工艺路线工序连线）
-        /// </summary>
-        private readonly IProcProcessRouteDetailLinkRepository _procProcessRouteDetailLinkRepository;
 
         /// <summary>
         /// 构造函数
         /// </summary>
         /// <param name="manuCommonService"></param>
-        /// <param name="procProcessRouteDetailNodeRepository"></param>
-        /// <param name="procProcessRouteDetailLinkRepository"></param>
-        public InStationJobService(IManuCommonService manuCommonService,
-            IProcProcessRouteDetailNodeRepository procProcessRouteDetailNodeRepository,
-            IProcProcessRouteDetailLinkRepository procProcessRouteDetailLinkRepository)
+        public InStationJobService(IManuCommonService manuCommonService)
         {
             _manuCommonService = manuCommonService;
-            _procProcessRouteDetailNodeRepository = procProcessRouteDetailNodeRepository;
-            _procProcessRouteDetailLinkRepository = procProcessRouteDetailLinkRepository;
         }
 
         /// <summary>
@@ -56,39 +36,7 @@ namespace Hymson.MES.CoreServices.Services.NewJob
         {
             if ((param is InStationRequestBo bo) == false) return;
 
-            // 校验工序和资源是否对应
-            var resourceIds = await param.Proxy.GetValueAsync(_manuCommonService.GetProcResourceIdByProcedureIdAsync, bo.ProcedureId);
-            if (resourceIds == null || resourceIds.Any(a => a == bo.ResourceId) == false) throw new CustomerValidationException(nameof(ErrorCode.MES16317));
-
-            // 获取生产条码信息
-            var sfcProduceEntities = await param.Proxy.GetValueAsync(_manuCommonService.GetProduceEntitiesBySFCsAsync, bo);
-            if (sfcProduceEntities == null || sfcProduceEntities.Any() == false) return;
-
-            var sfcProduceBusinessEntities = await param.Proxy.GetValueAsync(_manuCommonService.GetProduceBusinessEntitiesBySFCsAsync, bo);
-
-            // 合法性校验
-            sfcProduceEntities.VerifySFCStatus(SfcProduceStatusEnum.lineUp);
-            sfcProduceBusinessEntities?.VerifyProcedureLock(bo.SFCs, bo.ProcedureId);
-
-            // 验证条码是否被容器包装
-            await _manuCommonService.VerifyContainerAsync(bo);
-
-            // 如果工序对应不上
-            var firstProduceEntity = sfcProduceEntities.FirstOrDefault();
-            if (firstProduceEntity == null) return;
-
-            if (firstProduceEntity.ProcedureId != bo.ProcedureId)
-            {
-                var processRouteDetailLinks = await _procProcessRouteDetailLinkRepository.GetProcessRouteDetailLinksByProcessRouteIdAsync(firstProduceEntity.ProcessRouteId)
-                    ?? throw new CustomerValidationException(nameof(ErrorCode.MES18213));
-
-                var processRouteDetailNodes = await _procProcessRouteDetailNodeRepository.GetProcessRouteDetailNodesByProcessRouteIdAsync(firstProduceEntity.ProcessRouteId)
-                    ?? throw new CustomerValidationException(nameof(ErrorCode.MES18208));
-
-                // 判断上一个工序是否是随机工序
-                var IsRandomPreProcedure = await _manuCommonService.IsRandomPreProcedureAsync(processRouteDetailLinks, processRouteDetailNodes, firstProduceEntity.ProcessRouteId, bo.ProcedureId);
-                if (IsRandomPreProcedure == false) throw new CustomerValidationException(nameof(ErrorCode.MES16308));
-            }
+            await Task.CompletedTask;
         }
 
         /// <summary>
---
+++ HymsonMES/src/Libraries/Hymson.MES.CoreServices/Services/Job/InStationVerifyJobService.cs
@@ -0,0 +1,116 @@
+﻿using Hymson.Infrastructure.Exceptions;
+using Hymson.MES.Core.Attribute.Job;
+using Hymson.MES.Core.Constants;
+using Hymson.MES.Core.Enums;
+using Hymson.MES.Core.Enums.Job;
+using Hymson.MES.CoreServices.Bos.Job;
+using Hymson.MES.CoreServices.Services.Common.ManuCommon;
+using Hymson.MES.CoreServices.Services.Common.ManuExtension;
+using Hymson.MES.CoreServices.Services.Job;
+using Hymson.MES.Data.Repositories.Process;
+
+namespace Hymson.MES.CoreServices.Services.NewJob
+{
+    /// <summary>
+    /// 进站
+    /// </summary>
+    [Job("进站", JobTypeEnum.Standard)]
+    public class InStationVerifyJobService : IJobService
+    {
+        /// <summary>
+        /// 服务接口（生产通用）
+        /// </summary>
+        private readonly IManuCommonService _manuCommonService;
+
+        /// <summary>
+        /// 仓储接口（工艺路线工序节点）
+        /// </summary>
+        private readonly IProcProcessRouteDetailNodeRepository _procProcessRouteDetailNodeRepository;
+
+        /// <summary>
+        /// 仓储接口（工艺路线工序连线）
+        /// </summary>
+        private readonly IProcProcessRouteDetailLinkRepository _procProcessRouteDetailLinkRepository;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="manuCommonService"></param>
+        /// <param name="procProcessRouteDetailNodeRepository"></param>
+        /// <param name="procProcessRouteDetailLinkRepository"></param>
+        public InStationVerifyJobService(IManuCommonService manuCommonService,
+            IProcProcessRouteDetailNodeRepository procProcessRouteDetailNodeRepository,
+            IProcProcessRouteDetailLinkRepository procProcessRouteDetailLinkRepository)
+        {
+            _manuCommonService = manuCommonService;
+            _procProcessRouteDetailNodeRepository = procProcessRouteDetailNodeRepository;
+            _procProcessRouteDetailLinkRepository = procProcessRouteDetailLinkRepository;
+        }
+
+        /// <summary>
+        /// 参数校验
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public async Task VerifyParamAsync<T>(T param) where T : JobBaseBo
+        {
+            if ((param is InStationRequestBo bo) == false) return;
+
+            // 校验工序和资源是否对应
+            var resourceIds = await param.Proxy.GetValueAsync(_manuCommonService.GetProcResourceIdByProcedureIdAsync, bo.ProcedureId);
+            if (resourceIds == null || resourceIds.Any(a => a == bo.ResourceId) == false) throw new CustomerValidationException(nameof(ErrorCode.MES16317));
+
+            // 获取生产条码信息
+            var sfcProduceEntities = await param.Proxy.GetValueAsync(_manuCommonService.GetProduceEntitiesBySFCsAsync, bo);
+            if (sfcProduceEntities == null || sfcProduceEntities.Any() == false) return;
+
+            var sfcProduceBusinessEntities = await param.Proxy.GetValueAsync(_manuCommonService.GetProduceBusinessEntitiesBySFCsAsync, bo);
+
+            // 合法性校验
+            sfcProduceEntities.VerifySFCStatus(SfcProduceStatusEnum.lineUp);
+            sfcProduceBusinessEntities?.VerifyProcedureLock(bo.SFCs, bo.ProcedureId);
+
+            // 验证条码是否被容器包装
+            await _manuCommonService.VerifyContainerAsync(bo);
+
+            // 如果工序对应不上
+            var firstProduceEntity = sfcProduceEntities.FirstOrDefault();
+            if (firstProduceEntity == null) return;
+
+            if (firstProduceEntity.ProcedureId != bo.ProcedureId)
+            {
+                var processRouteDetailLinks = await _procProcessRouteDetailLinkRepository.GetProcessRouteDetailLinksByProcessRouteIdAsync(firstProduceEntity.ProcessRouteId)
+                    ?? throw new CustomerValidationException(nameof(ErrorCode.MES18213));
+
+                var processRouteDetailNodes = await _procProcessRouteDetailNodeRepository.GetProcessRouteDetailNodesByProcessRouteIdAsync(firstProduceEntity.ProcessRouteId)
+                    ?? throw new CustomerValidationException(nameof(ErrorCode.MES18208));
+
+                // 判断上一个工序是否是随机工序
+                var IsRandomPreProcedure = await _manuCommonService.IsRandomPreProcedureAsync(processRouteDetailLinks, processRouteDetailNodes, firstProduceEntity.ProcessRouteId, bo.ProcedureId);
+                if (IsRandomPreProcedure == false) throw new CustomerValidationException(nameof(ErrorCode.MES16308));
+            }
+        }
+
+        /// <summary>
+        /// 数据组装
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public async Task<TResult> DataAssemblingAsync<T, TResult>(T param) where T : JobBaseBo where TResult : JobResultBo, new()
+        {
+            await Task.CompletedTask;
+
+            return new TResult();
+        }
+
+        /// <summary>
+        /// 执行入库
+        /// </summary>
+        /// <returns></returns>
+        public async Task ExecuteAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+    }
+}
