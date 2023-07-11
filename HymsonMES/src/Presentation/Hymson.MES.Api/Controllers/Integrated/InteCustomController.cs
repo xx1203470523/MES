9e92e7796b1cc71350aca498f08bf6757cece9bd
longkai <longkai@hymson.com>
2023-07-11 15:02:50 +0800

    feat: 客户加上权限

--- HymsonMES/src/Presentation/Hymson.MES.Api/Controllers/Integrated/InteCustomController.cs
+++ HymsonMES/src/Presentation/Hymson.MES.Api/Controllers/Integrated/InteCustomController.cs
@@ -8,6 +8,7 @@
 using Hymson.Infrastructure;
 using Hymson.MES.Services.Dtos.Integrated;
 using Hymson.MES.Services.Services.Integrated;
+using Hymson.Web.Framework.Attributes;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -71,6 +72,8 @@ namespace Hymson.MES.Api.Controllers.Integrated
         /// <returns></returns>
         [HttpPost]
         [Route("create")]
+        [LogDescription("客户维护", BusinessType.INSERT)]
+        [PermissionDescription("inte:inteCustom:insert")]
         public async Task AddInteCustomAsync([FromBody] InteCustomCreateDto parm)
         {
              await _inteCustomService.CreateInteCustomAsync(parm);
@@ -83,6 +86,8 @@ namespace Hymson.MES.Api.Controllers.Integrated
         /// <returns></returns>
         [HttpPut]
         [Route("update")]
+        [LogDescription("客户维护", BusinessType.UPDATE)]
+        [PermissionDescription("inte:inteCustom:update")]
         public async Task UpdateInteCustomAsync([FromBody] InteCustomModifyDto parm)
         {
              await _inteCustomService.ModifyInteCustomAsync(parm);
@@ -95,6 +100,8 @@ namespace Hymson.MES.Api.Controllers.Integrated
         /// <returns></returns>
         [HttpDelete]
         [Route("delete")]
+        [LogDescription("客户维护", BusinessType.DELETE)]
+        [PermissionDescription("inte:inteCustom:delete")]
         public async Task DeleteInteCustomAsync([FromBody] long[] ids)
         {
             await _inteCustomService.DeletesInteCustomAsync(ids);
