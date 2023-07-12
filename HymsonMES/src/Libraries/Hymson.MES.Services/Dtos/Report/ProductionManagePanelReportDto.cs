0313f2df8715fd183761118d38578901bddd1756
Chenjx <chenjianxiong@hymson.com>
2023-07-12 16:55:33 +0800

    feat:生产综合数据测试

---
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Dtos/Report/ProductionManagePanelReportDto.cs
@@ -0,0 +1,87 @@
+﻿using Hymson.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymson.MES.Services.Dtos.Report
+{
+    public record ProductionManagePanelReportDto : BaseEntityDto
+    {
+        /// <summary>
+        /// 综合良率
+        /// </summary>
+        public decimal OverallYieldRate { get; set; }
+        /// <summary>
+        /// 计划达成率
+        /// </summary>
+        public decimal OverallPlanAchievingRate { get; set; }
+        /// <summary>
+        /// 日电芯消耗数量
+        /// 计算首工序进站电芯
+        /// </summary>
+        public int DayConsume { get; set; }
+        /// <summary>
+        /// 线体名称
+        /// </summary>
+        public string WorkLineName { get; set; }
+        /// <summary>
+        /// 完成率
+        /// </summary>
+        public decimal CompletedRate { get; set;}
+
+        /// <summary>
+        /// true白班，false夜班
+        /// </summary>
+        public bool DayShift { get; set; }
+        /// <summary>
+        /// 工艺路线编码
+        /// </summary>
+        public string ProcessRouteCode { get; set; }
+        /// <summary>
+        /// 工艺路线名称
+        /// </summary>
+        public string ProcessRouteName { get; set; }
+        /// <summary>
+        /// 工单下达时间
+        /// </summary>
+        public DateTime WorkOrderDownTime { get; set; }
+        /// <summary>
+        /// 工单号
+        /// </summary>
+        public string WorkOrderCode { get; set; }
+        /// <summary>
+        /// 工单数量
+        /// </summary>
+        public int WorkOrderQty { get; set; }
+
+        /// <summary>
+        /// 完工数量
+        /// </summary>
+        public int CompletedQty { get; set; }
+
+        /// <summary>
+        /// 不良数量
+        /// </summary>
+        public int UnqualifiedQty { get; set; }
+
+        /// <summary>
+        /// 不良率
+        /// </summary>
+        public int UnqualifiedRate { get; set; }
+
+        /// <summary>
+        /// 产品编码
+        /// </summary>
+        public string ProductCode { get; set; }
+        /// <summary>
+        /// 产品名称
+        /// </summary>
+        public string ProductName{ get; set; }
+        /// <summary>
+        /// 投入数量
+        /// </summary>
+        public int InputQty { get; set; }
+    }
+}
---
+++ HymsonMES/src/Presentation/Hymson.MES.Api/Controllers/Report/ProductionManagePanelController.cs
@@ -0,0 +1,48 @@
+﻿using Hymson.MES.Services.Dtos.Report;
+using Hymson.MES.Services.Services.Report;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hymson.MES.Api.Controllers.Report
+{
+    /// <summary>
+    /// 生产管理看板控制器
+    /// </summary>
+    [AllowAnonymous]
+    public class ProductionManagePanelController : ControllerBase
+    {
+        private readonly IProductTraceReportService _productTraceReportService;
+        public ProductionManagePanelController(IProductTraceReportService productTraceReportService)
+        {
+            _productTraceReportService = productTraceReportService;
+        }
+        /// <summary>
+        /// 获取综合信息
+        /// </summary>
+        /// <returns></returns>
+        public Task<ProductionManagePanelReportDto> GetOverallInfo()
+        {
+            //模拟数据
+            return Task.FromResult(new ProductionManagePanelReportDto()
+            {
+                CompletedQty = 100,
+                CompletedRate = 80,
+                DayConsume = 120,
+                DayShift = false,
+                InputQty = 120,
+                OverallPlanAchievingRate = 10,
+                OverallYieldRate = 90,
+                ProcessRouteCode = "1111",
+                ProcessRouteName = "工艺路线",
+                ProductCode = "AAA",
+                ProductName = "产品",
+                UnqualifiedQty = 10,
+                UnqualifiedRate = 10,
+                WorkLineName = "ajkfaw",
+                WorkOrderCode = "W292421AAFA",
+                WorkOrderDownTime = new DateTime(),
+                WorkOrderQty = 200
+            });
+        }
+    }
+}
