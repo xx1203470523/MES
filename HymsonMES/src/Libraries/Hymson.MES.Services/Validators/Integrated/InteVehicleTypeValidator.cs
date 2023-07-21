0619e1e3379269b788e65a4e95992a500d080f2b
longkai <longkai@hymson.com>
2023-07-21 08:54:34 +0800

    feat: 2607 【载具类型维护】载具类型维护的编辑页面-输入边界值校验，类型编码与名称输入框输入过长会报错

--- HymsonMES/src/Libraries/Hymson.MES.Services/Validators/Integrated/InteVehicleTypeValidator.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Validators/Integrated/InteVehicleTypeValidator.cs
@@ -22,8 +22,8 @@ namespace Hymson.MES.Services.Validators.Integrated
             RuleFor(x => x).NotEmpty().WithErrorCode(nameof(ErrorCode.MES10100));
             RuleFor(x => x.Code).NotEmpty().WithErrorCode(nameof(ErrorCode.MES18503));
             RuleFor(x => x.Name).NotEmpty().WithErrorCode(nameof(ErrorCode.MES18504));
-            RuleFor(x => x.Code).MaximumLength(50).WithErrorCode(nameof(ErrorCode.MES18505));
-            RuleFor(x => x.Name).MaximumLength(50).WithErrorCode(nameof(ErrorCode.MES18506));
+            RuleFor(x => x.Code).MaximumLength(100).WithErrorCode(nameof(ErrorCode.MES18505));
+            RuleFor(x => x.Name).MaximumLength(100).WithErrorCode(nameof(ErrorCode.MES18506));
             RuleFor(x => x.Remark).MaximumLength(255).WithErrorCode(nameof(ErrorCode.MES18507));
             RuleFor(x => x.Status).Must(it => Enum.IsDefined(typeof(EnableEnum), it)).WithErrorCode(nameof(ErrorCode.MES18508));
             RuleFor(x => x.Row).Must(x => x>0 ).WithErrorCode(nameof(ErrorCode.MES18509));
@@ -44,7 +44,7 @@ namespace Hymson.MES.Services.Validators.Integrated
             //RuleFor(x => x.Code).NotEmpty().WithErrorCode(nameof(ErrorCode.MES18503));
             RuleFor(x => x.Name).NotEmpty().WithErrorCode(nameof(ErrorCode.MES18504));
             //RuleFor(x => x.Code).MaximumLength(50).WithErrorCode(nameof(ErrorCode.MES18505));
-            RuleFor(x => x.Name).MaximumLength(50).WithErrorCode(nameof(ErrorCode.MES18506));
+            RuleFor(x => x.Name).MaximumLength(100).WithErrorCode(nameof(ErrorCode.MES18506));
             RuleFor(x => x.Remark).MaximumLength(255).WithErrorCode(nameof(ErrorCode.MES18507));
             RuleFor(x => x.Status).Must(it => Enum.IsDefined(typeof(EnableEnum), it)).WithErrorCode(nameof(ErrorCode.MES18508));
             RuleFor(x => x.Row).Must(x => x > 0).WithErrorCode(nameof(ErrorCode.MES18509));
