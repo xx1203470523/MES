3fef2b6729c1a20c98ff0c245efb95f5946413c6
wxk <wangxuke@hymson.com>
2023-07-21 11:07:33 +0800

    feat: 获取绑盘条码集合

--- HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Integrated/InteVehicleFreight/InteVehiceFreightStackQuery.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Integrated/InteVehicleFreight/InteVehiceFreightStackQuery.cs
@@ -16,11 +16,20 @@ namespace Hymson.MES.Data.Repositories.Integrated
         /// <summary>
         /// 载具Id
         /// </summary>
-        public long VehicleId { get; set; }
+        public long? VehicleId { get; set; }
+        /// <summary>
+        /// 位置Id
+        /// </summary>
+        public long? LocationId { get; set; }
+
         /// <summary>
         /// 站点Id
         /// </summary>
         public long SiteId { get; set; }
+        /// <summary>
+        /// 条码列表
+        /// </summary>
+        public IEnumerable<string> Sfcs { get; set; }
     }
     /// <summary>
     /// 二维载具条码明细 查询参数
