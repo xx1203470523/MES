179eb28ce175dfe3baab11da33d6404b892841d0
Chenjx <chenjianxiong@hymson.com>
2023-08-23 08:51:41 +0800

    fix:修改条码判断bug

--- HymsonMES/src/Libraries/Hymson.MES.Services/Services/Report/ManuProductParameterReport/ManuProductParameterReportService.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Services/Report/ManuProductParameterReport/ManuProductParameterReportService.cs
@@ -40,7 +40,7 @@ namespace Hymson.MES.Services.Services.Report.ManuProductParameterReport
         /// <returns></returns>
         public async Task<PagedInfo<ManuProductParameterReportViewDto>> GetManuProductParameterReportPageListAsync(ManuProductParameterReportPagedQueryDto pageQuery)
         {
-            if (pageQuery.SFCS == null || pageQuery.SFCS.Length <= 0 || string.IsNullOrEmpty(pageQuery.SFCStr))
+            if ((pageQuery.SFCS == null || pageQuery.SFCS.Length <= 0) && string.IsNullOrEmpty(pageQuery.SFCStr))
             {
                 throw new CustomerValidationException(nameof(ErrorCode.MES10111));
             }
@@ -61,7 +61,7 @@ namespace Hymson.MES.Services.Services.Report.ManuProductParameterReport
         /// <returns></returns>
         public async Task<ExportResultDto> ManuProductParameterReportExportAsync(ManuProductParameterReportPagedQueryDto pageQuery)
         {
-            if (pageQuery.SFCS == null || pageQuery.SFCS.Length <= 0 || string.IsNullOrEmpty(pageQuery.SFCStr))
+            if ((pageQuery.SFCS == null || pageQuery.SFCS.Length <= 0) && string.IsNullOrEmpty(pageQuery.SFCStr))
             {
                 throw new CustomerValidationException(nameof(ErrorCode.MES10111));
             }
