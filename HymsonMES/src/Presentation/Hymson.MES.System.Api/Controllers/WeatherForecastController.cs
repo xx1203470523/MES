725971bc4967ce2ceb40197e9e09250ef1a7f3ca
huangjiayun <huangjiayun@hymson.com>
2023-08-31 15:21:23 +0800

    feat:修改代码异味

--- HymsonMES/src/Presentation/Hymson.MES.System.Api/Controllers/WeatherForecastController.cs
+++ HymsonMES/src/Presentation/Hymson.MES.System.Api/Controllers/WeatherForecastController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Hymson.MES.System.Api.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
@@ -12,12 +15,18 @@ namespace Hymson.MES.System.Api.Controllers
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logger"></param>
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
