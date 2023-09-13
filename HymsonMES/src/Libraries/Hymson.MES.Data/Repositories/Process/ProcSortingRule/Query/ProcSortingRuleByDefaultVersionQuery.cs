25677c203449a78a10e760b5055124229f7ea44c
wangkeming <wangkeming@hymson.com>
2023-09-13 18:05:37 +0800

    fix: 修复分选规则所有bug

---
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Process/ProcSortingRule/Query/ProcSortingRuleByDefaultVersionQuery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymson.MES.Data.Repositories.Process.ProcSortingRule.Query
+{
+    /// <summary>
+    /// 查询当前版本的分选规则
+    /// </summary>
+    public class ProcSortingRuleByDefaultVersionQuery
+    {
+        /// <summary>
+        /// 站点id
+        /// </summary>
+        public long SiteId { get; set; }
+
+        /// <summary>
+        /// 编码
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
