b2a0372a844a3c482a6ae7b64c8ec6256211d189
SZHMX\huangjun1 <xpow@qq.com>
2023-10-07 14:10:26 +0800

    电芯进站校验同属批次功能

--- HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
@@ -1009,6 +1009,7 @@
 
         public const string MES19147 = "通过SFC:{SFC}和工单{OrderCode}没有找到在制信息";
         public const string MES19148 = "进站失败,{SFC}执行{action}时失败!";
+        public const string MES19149 = "进站异常不属于同一批次,{SFC}的批次为{sfcBatchNo},当前工单批次为{workBatchNo}";
         #endregion
 
         #region 系统对接 MES19201
