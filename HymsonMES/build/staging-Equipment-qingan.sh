903b99a68ae26acc926eda2cf300815f6267f50c
pengxin <pengxin@163.com>
2023-10-08 15:44:00 +0800

    feat: 清安测试环境构建设置

--- HymsonMES/build/staging-Equipment-qingan.sh
+++ HymsonMES/build/staging-Equipment-qingan.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 PublishEnvironment=Release
 timestamp=$(date +%Y%m%d%H%M%S)
-serviceName=hymson-mes-api-qinganyongtai-test
+serviceName=hymson-mes-equipment-api-qingan-test
 registryUrl=10.10.79.13:8081/test
-imageName=hymson.mes.api
-docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.Api/Dockerfile .
+imageName=hymson.mes.equipment.api
+docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.Equipment.Api/Dockerfile .
 docker tag $imageName:$timestamp  $registryUrl/$imageName:$timestamp
 docker push $registryUrl/$imageName:$timestamp
 docker service rm $serviceName
@@ -12,11 +12,10 @@ docker service create \
   --with-registry-auth \
   --name $serviceName \
   --mode global \
-  --publish mode=host,published=20024,target=80 \
+  --publish mode=host,published=20201,target=80 \
   --env ASPNETCORE_ENVIRONMENT=Staging \
   --env TZ="Asia/Shanghai" \
   --env SERVICE_CHECK_HTTP=/health \
   --env SERVICE_NAME={{.Service.Name}} \
-  --hostname="{{.Node.ID}}-{{.Service.Name}}" \
    --mount type=volume,src=hymsonvolume,dst=/logs \
   $registryUrl/$imageName:$timestamp
--- HymsonMES/build/staging-qingan.sh
+++ HymsonMES/build/staging-qingan.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 PublishEnvironment=Release
 timestamp=$(date +%Y%m%d%H%M%S)
-serviceName=hymson-mes-equipment-api-qinganyongtai-test
+serviceName=hymson-mes-api-qingan-test
 registryUrl=10.10.79.13:8081/test
-imageName=hymson.mes.equipment.api
-docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.Equipment.Api/Dockerfile .
+imageName=hymson.mes.api
+docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.Api/Dockerfile .
 docker tag $imageName:$timestamp  $registryUrl/$imageName:$timestamp
 docker push $registryUrl/$imageName:$timestamp
 docker service rm $serviceName
@@ -12,10 +12,11 @@ docker service create \
   --with-registry-auth \
   --name $serviceName \
   --mode global \
-  --publish mode=host,published=20029,target=80 \
+  --publish mode=host,published=20200,target=80 \
   --env ASPNETCORE_ENVIRONMENT=Staging \
   --env TZ="Asia/Shanghai" \
   --env SERVICE_CHECK_HTTP=/health \
   --env SERVICE_NAME={{.Service.Name}} \
+  --hostname="{{.Node.ID}}-{{.Service.Name}}" \
    --mount type=volume,src=hymsonvolume,dst=/logs \
   $registryUrl/$imageName:$timestamp
