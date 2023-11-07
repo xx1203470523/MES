8f9ced7e853059f74830323a8127cb4d67baac1d
kongaomeng <kongaomeng@hymson.com>
2023-11-07 16:50:06 +0800

    feat：bom导入字段更改

--- HymsonMES/src/Libraries/Hymson.MES.Services/Dtos/Process/ProcBomDto.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Dtos/Process/ProcBomDto.cs
@@ -226,7 +226,7 @@ namespace Hymson.MES.Services.Dtos.Process
         /// <summary>
         /// 是否当前版本
         /// </summary>
-        [EpplusTableColumn(Header = "是否为当前版本(0为不是/1为是)", Order = 4)]
+        [EpplusTableColumn(Header = "是否为当前版本(true/false)", Order = 4)]
         public bool IsCurrentVersion { get; set; }
 
 
