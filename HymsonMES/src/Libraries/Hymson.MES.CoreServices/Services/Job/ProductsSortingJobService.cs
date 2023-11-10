1814b0710ff25ae25488282d2f8b5bd0b994361f
zhao.qing <zhaoqing@hymson.com>
2023-11-10 18:08:46 +0800

    feat: 分选规则job

--- HymsonMES/src/Libraries/Hymson.MES.CoreServices/Services/Job/ProductsSortingJobService.cs
+++ HymsonMES/src/Libraries/Hymson.MES.CoreServices/Services/Job/ProductsSortingJobService.cs
@@ -1,15 +1,25 @@
 ﻿using Hymson.Infrastructure.Exceptions;
 using Hymson.MES.Core.Attribute.Job;
 using Hymson.MES.Core.Constants;
+using Hymson.MES.Core.Domain.Manufacture;
+using Hymson.MES.Core.Domain.Parameter;
+using Hymson.MES.Core.Domain.Process;
 using Hymson.MES.Core.Enums.Job;
+using Hymson.MES.Core.Enums.Process;
 using Hymson.MES.CoreServices.Bos.Common;
 using Hymson.MES.CoreServices.Bos.Job;
 using Hymson.MES.CoreServices.Services.Common.MasterData;
 using Hymson.MES.CoreServices.Services.Job;
+using Hymson.MES.CoreServices.Services.Parameter;
 using Hymson.MES.Data.Repositories.Manufacture;
+using Hymson.MES.Data.Repositories.Manufacture.ManuSfcGrade.Command;
 using Hymson.MES.Data.Repositories.Process;
+using Hymson.Snowflake;
+using Hymson.Utils;
+using IdGen;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,16 +32,6 @@ namespace Hymson.MES.CoreServices.Services.NewJob
     [Job("产品分选", JobTypeEnum.Standard)]
     public class ProductsSortingJobService : IJobService
     {
-        private readonly IManuSfcRepository _manuSfcRepository;
-
-        /// <summary>
-        /// 分选规则
-        /// </summary>
-        private readonly IProcSortingRuleRepository _sortingRuleRepository;
-        private readonly IProcSortingRuleDetailRepository _sortingRuleDetailRepository;
-        private readonly IProcSortingRuleGradeRepository _sortingRuleGradeRepository;
-        private readonly IProcSortingRuleGradeDetailsRepository _ruleGradeDetailsRepository;
-
         /// <summary>
         /// 条码档位表 仓储
         /// </summary>
@@ -42,19 +42,34 @@ namespace Hymson.MES.CoreServices.Services.NewJob
         /// </summary>
         private readonly IManuSfcGradeDetailRepository _gradeDetailRepository;
 
-        private readonly IMasterDataService _masterDataService;
+        /// <summary>
+        /// 分选规则详情
+        /// </summary>
+        private readonly IProcSortingRuleDetailRepository _sortingRuleDetailRepository;
+        private readonly IProcSortingRuleGradeRepository _sortingRuleGradeRepository;
+        private readonly IProcSortingRuleGradeDetailsRepository _ruleGradeDetailsRepository;
 
-        public ProductsSortingJobService(IManuSfcRepository manuSfcRepository,
-            IProcSortingRuleRepository sortingRuleRepository,
-            IManuSfcGradeRepository manuSfcGradeRepository,
+        private readonly IMasterDataService _masterDataService;
+        /// <summary>
+        ///产品参数采集
+        /// </summary>
+        private readonly IManuProductParameterService _manuProductParameterService;
+
+        public ProductsSortingJobService(IManuSfcGradeRepository manuSfcGradeRepository,
             IManuSfcGradeDetailRepository gradeDetailRepository,
-            IMasterDataService masterDataService)
+            IProcSortingRuleDetailRepository sortingRuleDetailRepository,
+            IProcSortingRuleGradeRepository sortingRuleGradeRepository,
+            IProcSortingRuleGradeDetailsRepository ruleGradeDetailsRepository,
+            IMasterDataService masterDataService,
+            IManuProductParameterService manuProductParameterService)
         {
-            _manuSfcRepository = manuSfcRepository;
-            _sortingRuleRepository = sortingRuleRepository;
             _manuSfcGradeRepository = manuSfcGradeRepository;
             _gradeDetailRepository = gradeDetailRepository;
+            _sortingRuleDetailRepository = sortingRuleDetailRepository;
+            _sortingRuleGradeRepository = sortingRuleGradeRepository;
+            _ruleGradeDetailsRepository = ruleGradeDetailsRepository;
             _masterDataService = masterDataService;
+            _manuProductParameterService = manuProductParameterService;
         }
 
         /// <summary>
@@ -92,14 +107,26 @@ namespace Hymson.MES.CoreServices.Services.NewJob
             {
                 SiteId = commonBo.SiteId,
                 Status = Core.Enums.SysDataStatusEnum.Enable,
-                IsDefaultVersion= true,
-                MaterialIds=productIds
+                IsDefaultVersion = true,
+                MaterialIds = productIds
             };
             var procSortingRules = await commonBo.Proxy.GetDataBaseValueAsync(_masterDataService.GetSortingRulesAsync, query);
             if (procSortingRules == null || !procSortingRules.Any())
             {
-
+                //
             }
+
+            //获取到条码的参数信息
+            //var parameterList = await _manuProductParameterService.GetProductParameterListBySFCAsync(new CoreServices.Dtos.Parameter.QueryParameterBySfcDto
+            //{
+            //    SiteId = commonBo.SiteId,
+            //    SFCs = sfcs
+            //});
+
+            //if (parameterList == null || !parameterList.Any())
+            //{
+            //    throw new CustomerValidationException(nameof(ErrorCode.MES14704));
+            //}
         }
 
         /// <summary>
@@ -151,10 +178,259 @@ namespace Hymson.MES.CoreServices.Services.NewJob
                 MaterialIds = productIds
             };
             var procSortingRules = await commonBo.Proxy.GetDataBaseValueAsync(_masterDataService.GetSortingRulesAsync, query);
+            if (procSortingRules == null || procSortingRules.Any() == false)
+            {
+                return default;
+            }
 
-            //为了不报错
-            await Task.CompletedTask;
+            //分选规则详细
+            var sortingRuleIds = procSortingRules.Select(x => x.Id).ToArray();
+            var sortingRuleDetailEntities = await _sortingRuleDetailRepository.GetProcSortingRuleDetailEntitiesAsync(new ProcSortingRuleDetailQuery
+            {
+                SiteId = commonBo.SiteId,
+                SortingRuleIds = sortingRuleIds
+            });
+            if (sortingRuleDetailEntities == null || !sortingRuleDetailEntities.Any())
+            {
+                return default;
+            }
+
+            sfcs = sfcProduceEntities.Select(x => x.SFC).ToList();
+            //获取到条码的参数信息
+            var parameterList = await _manuProductParameterService.GetProductParameterListBySFCAsync(new CoreServices.Dtos.Parameter.QueryParameterBySfcDto
+            {
+                SiteId = commonBo.SiteId,
+                SFCs = sfcs
+            });
+            if (parameterList == null || !parameterList.Any())
+            {
+                return default;
+            }
+
+            var insertGrades = new List<ManuSfcGradeEntity>();
+            var updateGrades = new List<UpdateGradeCommand>();
+            var gradeDetailEntities = new List<ManuSfcGradeDetailEntity>();
+
+            //读取条码的最终档位信息
+            var sfcGradeEntities = await _manuSfcGradeRepository.GetManuSfcGradeEntitiesAsync(new ManuSfcGradeQuery
+            {
+                SiteId = commonBo.SiteId,
+                Sfcs = sfcs.ToArray()
+            });
+
+            foreach (var sfc in sfcs)
+            {
+                var sfcParameterList = parameterList.Where(x => x.SFC == sfc);
+                if (sfcParameterList == null || !sfcParameterList.Any())
+                {
+                    continue;
+                }
+
+                //根据参数筛选过滤拿到最新的参数信息
+                var parameterEntities = sfcParameterList.GroupBy(x => x.ParameterId).Select(x => x.OrderByDescending(x => x.CreatedOn).First()).ToList();
+                var productId = sfcProduceEntities.FirstOrDefault(x => x.SFC == sfc)?.ProductId;
+                var sortRuleId = procSortingRules.FirstOrDefault(x => x.MaterialId == productId)?.Id;
+                var sortingRuleDetails = sortingRuleDetailEntities.Where(x => x.SortingRuleId == sortRuleId).ToList();
+                if (sortingRuleDetails == null || !sortingRuleDetails.Any())
+                {
+                    continue;
+                }
+
+                var grade = sfcGradeEntities.FirstOrDefault(x => x.SFC == sfc);
+                var gradeId = 0L;
+                if (grade == null)
+                {
+                    gradeId = IdGenProvider.Instance.CreateId();
+                    insertGrades.Add(new ManuSfcGradeEntity
+                    {
+                        Id = gradeId,
+                        SFC = sfc,
+                        //Grade = gradeSaveDto.Grade,
+                        SiteId = commonBo.SiteId,
+                        CreatedBy = commonBo.UserName,
+                        UpdatedBy = commonBo.UserName
+                    });
+                }
+                else
+                {
+                    gradeId = grade.Id;
+                    updateGrades.Add(new UpdateGradeCommand
+                    {
+                        Id = gradeId,
+                        //Grade = gradeSaveDto.Grade,
+                        UpdatedBy = commonBo.UserName,
+                        UpdatedOn = HymsonClock.Now()
+                    });
+                }
+
+                var sfcGradeDetails = new List<ManuSfcGradeDetailEntity>();
+                foreach (var parameter in parameterEntities)
+                {
+                    var ruleDetail = GetParameterRating(parameter, sortingRuleDetailEntities);
+                    sfcGradeDetails.Add(new ManuSfcGradeDetailEntity
+                    {
+                        Id = IdGenProvider.Instance.CreateId(),
+                        SiteId = commonBo.SiteId,
+                        GadeId = gradeId,
+                        ProduceId = parameter.ProcedureId,
+                        SFC = sfc,
+                        Grade = ruleDetail?.Rating ?? "",
+                        ParamId = parameter.Id,
+                        ParamValue = parameter.ParameterValue,
+                        MaxValue = ruleDetail?.MaxValue ?? 0,
+                        MinValue = ruleDetail?.MinValue ?? 0,
+                        MinContainingType = ruleDetail?.MinContainingType,
+                        MaxContainingType = ruleDetail?.MaxContainingType,
+                        CreatedBy = commonBo.UserName,
+                        UpdatedBy = commonBo.UserName,
+                    });
+                }
+
+                if (sfcGradeDetails.Any())
+                {
+                    gradeDetailEntities.AddRange(sfcGradeDetails);
+                    //根据组合拿到sfc的最终档次信息
+                    // string grade = await GetFinalGrade(sortRuleId, SfcParamters);
+                }
+            }
+
             return default;
+        }
+
+        /// <summary>
+        /// 根据参数和规则获取参数的等级信息
+        /// </summary>
+        /// <param name="parameter">产品参数</param>
+        /// <param name="sortingRuleDetailEntities">分选规则</param>
+        /// <returns></returns>
+        private ProcSortingRuleDetailEntity GetParameterRating(ManuProductParameterEntity parameter, IEnumerable<ProcSortingRuleDetailEntity> sortingRuleDetailEntities)
+        {
+            var procSortingRuleDetail = new ProcSortingRuleDetailEntity
+            {
+                Id = 0,
+                Rating = ""
+            };
+
+            if (string.IsNullOrWhiteSpace(parameter.ParameterValue))
+            {
+                return procSortingRuleDetail;
+            }
+
+            var procSortingRules = sortingRuleDetailEntities.Where(x => x.ParameterId == parameter.ParameterId && x.ProcedureId == parameter.ProcedureId).ToList();
+            if (procSortingRules == null || !procSortingRules.Any())
+            {
+                return procSortingRuleDetail;
+            }
+
+            var parameterValue = parameter.ParameterValue.ParseToDecimal();
+            //先查找固定值的
+            var ruleDetail = procSortingRules.FirstOrDefault(x => x.ParameterValue == parameter.ParameterValue.ParseToDecimal());
+            if (ruleDetail != null)
+            {
+                return ruleDetail;
+            }
+
+            //再查找范围在这个范围内的,根据类型取<还是小于等于
+            procSortingRules = procSortingRules.Where(x => x.MinContainingType.HasValue && x.MaxContainingType.HasValue).ToList();
+            if (procSortingRules == null || !procSortingRules.Any())
+            {
+                return procSortingRuleDetail;
+            }
+
+            foreach (var rule in procSortingRules)
+            {
+                ruleDetail = new ProcSortingRuleDetailEntity();
+                if (rule.MinContainingType == ContainingTypeEnum.Lt && rule.MinValue < parameterValue)
+                {
+                    if (rule.MaxContainingType == ContainingTypeEnum.Lt && rule.MaxValue > parameterValue)
+                    {
+                        ruleDetail = rule;
+                    }
+
+                    if (rule.MaxContainingType == ContainingTypeEnum.LtOrE && rule.MaxValue >= parameterValue)
+                    {
+                        ruleDetail = rule;
+                    }
+                }
+
+                if (rule.MinContainingType == ContainingTypeEnum.LtOrE && rule.MinValue <= parameterValue)
+                {
+                    if (rule.MaxContainingType == ContainingTypeEnum.Lt && rule.MaxValue > parameterValue)
+                    {
+                        ruleDetail = rule;
+                    }
+
+                    if (rule.MaxContainingType == ContainingTypeEnum.LtOrE && rule.MaxValue >= parameterValue)
+                    {
+                        ruleDetail = rule;
+                    }
+                }
+
+                if (ruleDetail != null && ruleDetail.Id > 0)
+                {
+                    break;
+                }
+            }
+
+            if (ruleDetail != null)
+            {
+                return ruleDetail;
+            }
+            return procSortingRuleDetail;
+        }
+
+        /// <summary>
+        /// 根据组合拿到电芯最终档位信息
+        /// </summary>
+        /// <param name="sortingRuleId"></param>
+        /// <param name="SfcParamters"></param>
+        /// <returns></returns>
+        private async Task<string> GetFinalGrade(long sortingRuleId, List<ManuSfcGradeDetailEntity> sfcParamters)
+        {
+            if (sfcParamters == null || !sfcParamters.Any())
+            {
+                return string.Empty;
+            }
+
+            if (sfcParamters.Any(x => string.IsNullOrWhiteSpace(x.Grade)))
+            {
+                return string.Empty;
+            }
+
+            var ruleGradeEntities = await _sortingRuleGradeRepository.GetSortingRuleGradesByIdAsync(sortingRuleId);
+            if (ruleGradeEntities == null || !ruleGradeEntities.Any())
+            {
+                return string.Empty;
+            }
+
+            var ruleGradeDetailsEntities = await _ruleGradeDetailsRepository.GetSortingRuleGradeeDetailsByIdAsync(sortingRuleId);
+            if (ruleGradeDetailsEntities == null || !ruleGradeDetailsEntities.Any())
+            {
+                return string.Empty;
+            }
+
+            var ruleDetailIds = sfcParamters.Select(a => a.Id).ToArray();
+            var ruleGradeDetails = ruleGradeDetailsEntities.Where(x => ruleDetailIds.Contains(x.SortingRuleDetailId));
+            if (ruleGradeDetails == null || !ruleGradeDetails.Any())
+            {
+                return string.Empty;
+            }
+
+            var sortingRuleDetails = await _sortingRuleDetailRepository.GetSortingRuleDetailByIdAsync(sortingRuleId);
+            //List<SortingRuleGradeDto> list = new();
+            //foreach (var item in ruleGradeEntities)
+            //{
+            //    var gradeDetails = ruleGradeDetailsEntities.Where(x => x.SortingRuleGradeId == item.Id);
+            //    list.Add(new SortingRuleGradeDto
+            //    {
+            //        Grade = item.Grade,
+            //        Remark = item.Remark,
+            //        Ratings = sortingRuleDetails.Where(o => gradeDetails.Select(x => x.SortingRuleDetailId).Contains(o.Id)).Select(k => k.Rating)
+            //    });
+            //}
+            //var rating = string.Join(" ", sortingRuleDetails.Where(o => ruleDetailIds.Contains(o.Id)).Select(k => k.Rating).ToArray());
+            //return list.FirstOrDefault(x => string.Join(" ", x.Ratings) == rating)?.Grade ?? "";
+            return "";
         }
 
         /// <summary>
