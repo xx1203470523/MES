7d905d7ad3369d41eef09d1abb2ebf0471cccbba
huangjiayun <huangjiayun@hymson.com>
2023-12-21 09:38:55 +0800

    feat：配方操作

--- HymsonMES/src/Libraries/Hymson.MES.Services/Validators/Process/ProcFormulaOperationGroupValidator.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Validators/Process/ProcFormulaOperationGroupValidator.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using Hymson.MES.Core.Constants;
 using Hymson.MES.Services.Dtos.Process;
 
 namespace Hymson.MES.Services.Validators.Process
@@ -6,12 +7,14 @@ namespace Hymson.MES.Services.Validators.Process
     /// <summary>
     /// 配方操作组 验证
     /// </summary>
-    internal class ProcFormulaOperationGroupSaveValidator: AbstractValidator<ProcFormulaOperationGroupSaveDto>
+    internal class ProcFormulaOperationGroupSaveValidator : AbstractValidator<ProcFormulaOperationGroupSaveDto>
     {
         public ProcFormulaOperationGroupSaveValidator()
         {
-            //RuleFor(x => x.BatchNo).NotEmpty().WithErrorCode("11");
-            //RuleFor(x => x.BatchNo).MaximumLength(10).WithErrorCode("111");
+            RuleFor(x => x.Code).NotEmpty().WithErrorCode(nameof(ErrorCode.MES10113));
+            RuleFor(x => x.Code).MaximumLength(50).WithErrorCode(nameof(ErrorCode.MES10109));
+            RuleFor(x => x.Name).NotEmpty().WithErrorCode(nameof(ErrorCode.MES10116));
+            RuleFor(x => x.Name).MaximumLength(50).WithErrorCode(nameof(ErrorCode.MES10110));
         }
     }
 
--- HymsonMES/src/Libraries/Hymson.MES.Services/Validators/Process/ProcFormulaOperationValidator.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Validators/Process/ProcFormulaOperationValidator.cs
@@ -1,4 +1,7 @@
 using FluentValidation;
+using Hymson.MES.Core.Constants;
+using Hymson.MES.Core.Enums;
+using Hymson.MES.Core.Enums.Process;
 using Hymson.MES.Services.Dtos.Process;
 
 namespace Hymson.MES.Services.Validators.Process
@@ -6,12 +9,17 @@ namespace Hymson.MES.Services.Validators.Process
     /// <summary>
     /// 配方操作 验证
     /// </summary>
-    internal class ProcFormulaOperationSaveValidator: AbstractValidator<ProcFormulaOperationSaveDto>
+    internal class ProcFormulaOperationSaveValidator : AbstractValidator<ProcFormulaOperationSaveDto>
     {
         public ProcFormulaOperationSaveValidator()
         {
-            //RuleFor(x => x.BatchNo).NotEmpty().WithErrorCode("11");
-            //RuleFor(x => x.BatchNo).MaximumLength(10).WithErrorCode("111");
+            RuleFor(x => x.Code).NotEmpty().WithErrorCode(nameof(ErrorCode.MES10113));
+            RuleFor(x => x.Code).MaximumLength(50).WithErrorCode(nameof(ErrorCode.MES10109));
+            RuleFor(x => x.Name).NotEmpty().WithErrorCode(nameof(ErrorCode.MES10116));
+            RuleFor(x => x.Name).MaximumLength(50).WithErrorCode(nameof(ErrorCode.MES10110));
+            RuleFor(x => x.Version).NotEmpty().Must(it => it != "").WithErrorCode(nameof(ErrorCode.MES10618));
+
+            RuleFor(x => x.Type).Must(it => Enum.IsDefined(typeof(FormulaOperationTypeEnum), it)).WithErrorCode("配方操作类型不合法");
         }
     }
 
