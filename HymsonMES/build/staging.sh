313e86f0b10078575d24b63aad17cfab4abc4ce1
wangkeming <wangkeming@hymson.com>
2023-12-25 14:44:18 +0800

    feat: 还原部署文件

--- HymsonMES/build/staging.sh
+++ HymsonMES/build/staging.sh
@@ -1,21 +1,19 @@
 #! /bin/bash
 PublishEnvironment=Release
 timestamp=$(date +%Y%m%d%H%M%S)
-serviceName=hymson-mes-battery-api-test
+serviceName=hymson-mes-backgroundtasks-test
 registryUrl=10.10.79.13:8081/test
-imageName=hymson.mes.api
-docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.Api/Dockerfile .
+imageName=hymson.mes.backgroundtasks
+docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.BackgroundTasks/Dockerfile .
 docker tag $imageName:$timestamp  $registryUrl/$imageName:$timestamp
 docker push $registryUrl/$imageName:$timestamp
 docker service rm $serviceName
 docker service create \
   --with-registry-auth \
   --name $serviceName \
-  --mode global \
-  --publish mode=host,published=20723,target=80 \
-  --env ASPNETCORE_ENVIRONMENT=Staging \
+  --replicas 2 \
+  --env DOTNET_ENVIRONMENT=Staging \
   --env TZ="Asia/Shanghai" \
-  --env SERVICE_CHECK_HTTP=/health \
   --env SERVICE_NAME={{.Service.Name}} \
   --hostname="{{.Node.ID}}-{{.Service.Name}}"\
    --mount type=volume,src=hymsonvolume,dst=/logs \
