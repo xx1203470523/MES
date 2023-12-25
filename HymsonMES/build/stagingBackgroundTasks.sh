7792e80f5207acdec00b4c3e72665bbb24f94d21
Czhipu <czhipu@126.com>
2023-12-25 15:21:32 +0800

    feat: test

--- HymsonMES/build/stagingBackgroundTasks.sh
+++ HymsonMES/build/stagingBackgroundTasks.sh
@@ -7,14 +7,8 @@ imageName=hymson.mes.backgroundtasks
 docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.BackgroundTasks/Dockerfile .
 docker tag $imageName:$timestamp  $registryUrl/$imageName:$timestamp
 docker push $registryUrl/$imageName:$timestamp
-echo '1'
-echo $serviceName
-echo '2'
 docker service rm $serviceName
-echo '3'
-echo $imageName
-echo $timestamp
-echo '4'
+echo $registryUrl/$imageName:$timestamp
 docker service create \
   --with-registry-auth \
   --name $serviceName \
