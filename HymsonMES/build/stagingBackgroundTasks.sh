112e1306e6a9ff8c59e2ed052463f738ab3acb79
Czhipu <czhipu@126.com>
2023-12-25 14:35:38 +0800

    feat: test

--- HymsonMES/build/stagingBackgroundTasks.sh
+++ HymsonMES/build/stagingBackgroundTasks.sh
@@ -3,7 +3,7 @@ PublishEnvironment=Release
 timestamp=$(date +%Y%m%d%H%M%S)
 serviceName=hymson-mes-battery-backgroundtasks-test
 registryUrl=10.10.79.13:8081/test
-imageName=hymson.mes.battery.backgroundtasks
+imageName=hymson.mes.backgroundtasks
 docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.BackgroundTasks/Dockerfile .
 docker tag $imageName:$timestamp  $registryUrl/$imageName:$timestamp
 docker push $registryUrl/$imageName:$timestamp
