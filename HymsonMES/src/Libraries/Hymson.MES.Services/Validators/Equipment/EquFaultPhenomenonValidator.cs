1dcff4a74be1e9d420173065e09686f370857b70
Czhipu <czhipu@126.com>
2023-12-28 09:27:22 +0800

    feat: bugfix 3911 3923 3939

--- HymsonMES/src/Libraries/Hymson.MES.Services/Validators/Equipment/EquFaultPhenomenonValidator.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Validators/Equipment/EquFaultPhenomenonValidator.cs
@@ -16,7 +16,7 @@ namespace Hymson.MES.Services.Validators.Equipment
         {
             RuleFor(x => x.Code).NotEmpty().WithErrorCode(ErrorCode.MES10113);
             RuleFor(x => x.Code).MaximumLength(50).WithErrorCode(ErrorCode.MES10109);
-            RuleFor(x => x.Name).Matches("^[a-zA-Z0-9]+$").WithErrorCode(nameof(ErrorCode.MES10131));
+            RuleFor(x => x.Code).Matches("^[^`~!@#$%^&*()+<>?:\\\"\\'{},.\\/;\\[\\]\\\\]+$").WithErrorCode(nameof(ErrorCode.MES10132));
             RuleFor(x => x.Name).NotEmpty().WithErrorCode(ErrorCode.MES10116);
             RuleFor(x => x.Name).MaximumLength(50).WithErrorCode(ErrorCode.MES10110);
 
