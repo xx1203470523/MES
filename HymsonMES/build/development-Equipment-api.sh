76a7fc459383b7ffdfea74fef5f9978027fc4809
wangkeming <wangkeming998@163.com>
2024-02-20 10:45:07 +0800

    feat: 环境信息

--- HymsonMES/build/development-Equipment-api.sh
+++ HymsonMES/build/development-Equipment-api.sh
@@ -1,20 +1,21 @@
 #! /bin/bash
 PublishEnvironment=Debug
 timestamp=$(date +%Y%m%d%H%M%S)
-serviceName=hymson-mes-backgroundtasks-qkny-dev
+serviceName=hymson-mes-qkny-equipment-api-dev
 registryUrl=harbor.xnebula.com/new-energy
-imageName=hymson.mes.backgroundtasks
-docker build --build-arg PublishEnvironment=$PublishEnvironment -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.BackgroundTasks/Dockerfile .
+imageName=hymson.mes.equipment.api
+docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.Equipment.Api/Dockerfile .
 docker tag $imageName:$timestamp  $registryUrl/$imageName:$timestamp
 docker push $registryUrl/$imageName:$timestamp
 docker service rm $serviceName
 docker service create \
   --with-registry-auth \
   --name $serviceName \
-  --replicas 2 \
-  --env DOTNET_ENVIRONMENT=Development \
+  --mode global \
+  --publish mode=host,published=10333,target=80 \
+  --env ASPNETCORE_ENVIRONMENT=Staging \
   --env TZ="Asia/Shanghai" \
+  --env SERVICE_CHECK_HTTP=/health \
   --env SERVICE_NAME={{.Service.Name}} \
-  --hostname="{{.Node.ID}}-{{.Service.Name}}"\
    --mount type=volume,src=hymsonvolume,dst=/logs \
   $registryUrl/$imageName:$timestamp
