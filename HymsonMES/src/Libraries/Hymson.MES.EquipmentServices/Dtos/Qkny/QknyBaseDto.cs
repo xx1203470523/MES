77e7d5a3d0b12ee4f05cb1a2dfab0fc4650f6f51
yuan.xixi <szxx809@20240201>
2024-02-27 21:39:33 +0800

    feat: 设备接口初始化 1. 屏蔽hostedService里面启动时运行，防止本地启动报错 2. 修改配置文件，设备为任意地址可以访问，方便调试 3. 新增操作员登录，进站多个，时间同步多个接口

---
+++ HymsonMES/src/Libraries/Hymson.MES.EquipmentServices/Dtos/Qkny/QknyBaseDto.cs
@@ -0,0 +1,42 @@
+﻿using Hymson.Infrastructure;
+
+namespace Hymson.MES.EquipmentServices
+{
+    /// <summary>
+    /// 顷刻能源设备对接基础类
+    /// </summary>
+    public record QknyBaseDto : BaseEntityDto
+    {
+        /// <summary>
+        /// 设备编码
+        /// </summary>
+        public string EquipmentCode { get; set; } = "";
+
+        /// <summary>
+        /// 资源编码
+        /// </summary>
+        public string ResourceCode { get; set; } = "";
+
+        /// <summary>
+        /// 设备调用本地时间
+        /// </summary>
+        public DateTime LocalTime { get; set; }
+    }
+
+    /// <summary>
+    /// 顷刻能源设备对接返回结果基础类
+    /// </summary>
+    public record QknyReturnBaseDto
+    {
+        /// <summary>
+        /// 返回码
+        /// </summary>
+        public int Code { get; set; }
+
+        /// <summary>
+        /// 返回消息
+        /// </summary>
+        public string Msg { get; set; } = "";
+    }
+
+}
---
+++ HymsonMES/src/Presentation/Hymson.MES.Equipment.Api/Controllers/QknyController.cs
@@ -0,0 +1,74 @@
+﻿using Hymson.Infrastructure.Exceptions;
+using Hymson.MES.Core.Constants;
+using Hymson.MES.EquipmentServices;
+using Hymson.MES.EquipmentServices.Dtos.Qkny.Common;
+using Hymson.MES.EquipmentServices.Dtos.Qkny.Manufacture;
+using Hymson.Utils;
+using Hymson.Web.Framework.Attributes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hymson.MES.Equipment.Api.Controllers
+{
+    /// <summary>
+    /// 顷刻能源设备接口控制器
+    /// </summary>
+    [ApiController]
+    [Route("QknyEqu/api/v1")]
+    public class QknyController : ControllerBase
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public QknyController()
+        {
+
+        }
+
+        /// <summary>
+        /// 操作员登录
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("OperatorLogin")]
+        [LogDescription("操作员登录", BusinessType.OTHER, "OperatorLoginMes001", ReceiverTypeEnum.MES)]
+        public async Task OperatorLoginAsync(OperationLoginDto dto)
+        {
+            List<string> list = null;
+            int count = list.Count;
+
+            //throw new CustomerValidationException(nameof(ErrorCode.MES10100));
+        }
+
+        /// <summary>
+        /// 时间同步
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("TimeSynch")]
+        [LogDescription("时间同步", BusinessType.OTHER, "TimeSynchMes002", ReceiverTypeEnum.MES)]
+        public async Task<string> TimeSynchAsync(QknyBaseDto dto)
+        {
+            DateTime date = HymsonClock.Now();
+
+            return date.ToString("yyyy-MM-dd HH:mm:ss.fff");
+        }
+
+        /// <summary>
+        /// 进站多个
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("InboundMore")]
+        [LogDescription("进站多个", BusinessType.OTHER, "TimeSynchMes010", ReceiverTypeEnum.MES)]
+        public async Task<InboundMoreReturnDto> InboundMoreAsync(InboundMoreDto dto)
+        {
+            InboundMoreReturnDto result = new InboundMoreReturnDto();
+
+            return result;
+        }
+    }
+}
