402867591d14eecea956195e1b078d2be215ddb9
Czhipu <czhipu@126.com>
2024-03-08 11:04:47 +0800

    feat: add files

---
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Quality/QualIqcOrderSample/IQualIqcOrderSampleRepository.cs
@@ -0,0 +1,84 @@
+using Hymson.Infrastructure;
+using Hymson.MES.Core.Domain.Quality;
+using Hymson.MES.Data.Repositories.Common.Command;
+using Hymson.MES.Data.Repositories.Quality.Query;
+
+namespace Hymson.MES.Data.Repositories.Quality
+{
+    /// <summary>
+    /// 仓储接口（Iqc样本）
+    /// </summary>
+    public interface IQualIqcOrderSampleRepository
+    {
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<int> InsertAsync(QualIqcOrderSampleEntity entity);
+        
+        /// <summary>
+        /// 新增（批量）
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        Task<int> InsertRangeAsync(IEnumerable<QualIqcOrderSampleEntity> entities);
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<int> UpdateAsync(QualIqcOrderSampleEntity entity);
+        
+        /// <summary>
+        /// 更新（批量）
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        Task<int> UpdateRangeAsync(IEnumerable<QualIqcOrderSampleEntity> entities);
+
+        /// <summary>
+        /// 软删除  
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<int> DeleteAsync(long id);
+        
+        /// <summary>
+        /// 软删除（批量）
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        Task<int> DeletesAsync(DeleteCommand command);
+
+        /// <summary>
+        /// 根据ID获取数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<QualIqcOrderSampleEntity> GetByIdAsync(long id);
+    
+        /// <summary>
+        /// 根据IDs获取数据（批量）
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        Task<IEnumerable<QualIqcOrderSampleEntity>> GetByIdsAsync(long[] ids);
+
+        /// <summary>
+        /// 获取List
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        Task<IEnumerable<QualIqcOrderSampleEntity>> GetEntitiesAsync(QualIqcOrderSampleQuery query);
+        
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <param name="pagedQuery"></param>
+        /// <returns></returns>
+        Task<PagedInfo<QualIqcOrderSampleEntity>> GetPagedListAsync(QualIqcOrderSamplePagedQuery pagedQuery);
+
+    }
+}
---
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Quality/QualIqcOrderType/IQualIqcOrderTypeRepository.cs
@@ -0,0 +1,84 @@
+using Hymson.Infrastructure;
+using Hymson.MES.Core.Domain.Quality;
+using Hymson.MES.Data.Repositories.Common.Command;
+using Hymson.MES.Data.Repositories.Quality.Query;
+
+namespace Hymson.MES.Data.Repositories.Quality
+{
+    /// <summary>
+    /// 仓储接口（IQC检验类型）
+    /// </summary>
+    public interface IQualIqcOrderTypeRepository
+    {
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<int> InsertAsync(QualIqcOrderTypeEntity entity);
+        
+        /// <summary>
+        /// 新增（批量）
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        Task<int> InsertRangeAsync(IEnumerable<QualIqcOrderTypeEntity> entities);
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<int> UpdateAsync(QualIqcOrderTypeEntity entity);
+        
+        /// <summary>
+        /// 更新（批量）
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        Task<int> UpdateRangeAsync(IEnumerable<QualIqcOrderTypeEntity> entities);
+
+        /// <summary>
+        /// 软删除  
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<int> DeleteAsync(long id);
+        
+        /// <summary>
+        /// 软删除（批量）
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        Task<int> DeletesAsync(DeleteCommand command);
+
+        /// <summary>
+        /// 根据ID获取数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<QualIqcOrderTypeEntity> GetByIdAsync(long id);
+    
+        /// <summary>
+        /// 根据IDs获取数据（批量）
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        Task<IEnumerable<QualIqcOrderTypeEntity>> GetByIdsAsync(long[] ids);
+
+        /// <summary>
+        /// 获取List
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        Task<IEnumerable<QualIqcOrderTypeEntity>> GetEntitiesAsync(QualIqcOrderTypeQuery query);
+        
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <param name="pagedQuery"></param>
+        /// <returns></returns>
+        Task<PagedInfo<QualIqcOrderTypeEntity>> GetPagedListAsync(QualIqcOrderTypePagedQuery pagedQuery);
+
+    }
+}
