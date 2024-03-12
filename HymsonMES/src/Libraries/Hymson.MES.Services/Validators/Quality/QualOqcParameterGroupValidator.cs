cc7a8ef85d83498b2c2e6a52a98003ee25962f91
Jam <xpow@qq.com>
2024-03-12 09:53:00 +0800

    OQC更改验证器

--- HymsonMES/src/Libraries/Hymson.MES.Services/Validators/Quality/QualOqcParameterGroupValidator.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Validators/Quality/QualOqcParameterGroupValidator.cs
@@ -1,4 +1,6 @@
 using FluentValidation;
+using Hymson.MES.Core.Constants;
+using Hymson.MES.Services.Dtos.Qual;
 using Hymson.MES.Services.Dtos.Quality;
 
 namespace Hymson.MES.Services.Validators.Quality
@@ -6,7 +8,7 @@ namespace Hymson.MES.Services.Validators.Quality
     /// <summary>
     /// OQC检验参数组 验证
     /// </summary>
-    internal class QualOqcParameterGroupSaveValidator : AbstractValidator<QualOqcParameterGroupSaveDto>
+    internal class QualOqcParameterGroupSaveValidator : AbstractValidator<QualOqcParameterGroupDto>
     {
         /// <summary>
         /// 
@@ -15,6 +17,34 @@ namespace Hymson.MES.Services.Validators.Quality
         {
             //RuleFor(x => x.Code).NotEmpty().WithErrorCode("11");
             //RuleFor(x => x.BatchNo).MaximumLength(10).WithErrorCode("111");
+        }
+    }
+
+ 
+
+    /// <summary>
+    /// OQC检验项目 ; 更新校验器
+    /// 描述：
+    /// </summary>
+    /// <returns></returns>
+    internal class QualOqcParameterGroupUpdateValidator : AbstractValidator<QualOqcParameterGroupUpdateDto>
+    {
+        public QualOqcParameterGroupUpdateValidator()
+        {
+            RuleFor(m => m.Id).NotEmpty().WithErrorCode(nameof(ErrorCode.MES10130));
+        }
+    }
+
+    /// <summary>
+    /// OQC检验项目 ; 删除校验器
+    /// 描述：
+    /// </summary>
+    /// <returns></returns>
+    internal class QualOqcParameterGroupDeleteValidator : AbstractValidator<QualOqcParameterGroupDeleteDto>
+    {
+        public QualOqcParameterGroupDeleteValidator()
+        {
+            RuleFor(m => m.Ids).NotEmpty().WithErrorCode(nameof(ErrorCode.MES10130));
         }
     }
 
