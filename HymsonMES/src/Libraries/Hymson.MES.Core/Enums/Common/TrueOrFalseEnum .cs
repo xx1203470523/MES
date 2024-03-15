55c5144279f11f6a1a4e8ec9f87d76bc20bfb4ab
wangkeming <wangkeming998@163.com>
2024-03-15 11:58:05 +0800

    feat: 测试

--- HymsonMES/src/Libraries/Hymson.MES.Core/Enums/Common/TrueOrFalseEnum .cs
+++ HymsonMES/src/Libraries/Hymson.MES.Core/Enums/Common/TrueOrFalseEnum .cs
@@ -8,7 +8,7 @@ namespace Hymson.MES.Core.Enums
     public enum TrueOrFalseEnum : sbyte
     {
         /// <summary>
-        /// 是1
+        /// 是
         /// </summary>
         [Description("是")]
         Yes = 1,
