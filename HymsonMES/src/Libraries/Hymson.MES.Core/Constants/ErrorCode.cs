be3129411b9dbd055a2656d52a06461ad79ce974
Jam <xpow@qq.com>
2024-03-19 17:26:00 +0800

    4427 【IQC检验项目、OQC检验项目】详情、编辑界面，检验类型的值没有显示出来 4436 【OQC检验项目】检验项目编码重复，数据新增成功 4437 【OQC检验项目】客户 + 物料 + 检验项目版本的值不唯一，数据新增成功 4440 【OQC检验项目】新增界面，在检验项目弹框中，输入错误的物料名称进行查询，查询结果错误 4442 【OQC检验项目】主界面搜索栏，物料编码、物料名称、客户名称，在这三个输入框中输入错误的值进行查询，查询结果错误 4443 【编码规则】重复新增IQC编码类型，新增成功 4444 【编码规则】重复新增OQC编码类型，新增成功

--- HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Core/Constants/ErrorCode.cs
@@ -560,6 +560,7 @@
         public const string MES12448 = "包装序列码的容器编码不可为空";
         public const string MES12449 = "包装序列码必须填写有效的容器编码";
         public const string MES12450 = "容器{code}编码规则已存在，请删除原规则再进行新增!";
+        public const string MES12451 = "编码类型{type}已经存在记录";
         #endregion
 
         #region 容器维护 12500
@@ -1900,6 +1901,8 @@
         public const string MES19952 = "OQC检验项目必须填写物料，且物料信息必须准确";
         public const string MES19953 = "已启用的检验项目不允许删除，{codes}";
         public const string MES19954 = "检验项目不存在，可能操作时被删除或数据异常";
+        public const string MES19955 = "检验项目编码重复，数据新增失败";
+        public const string MES19956 = "客户{customCode}物料{materialCode}检验项目版本{version}已存在，不允许重复添加！";
 
         #endregion 
 
