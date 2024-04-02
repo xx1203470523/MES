dc08a7e77ded5fea1d18a4eb82827d887e4572d9
ben_liu <ben_liu123456>
2024-04-02 13:59:36 +0800

    转换系数添加Remark信息保存

--- HymsonMES/src/Libraries/Hymson.MES.Services/Dtos/Process/ProcConversionFactorDto.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Dtos/Process/ProcConversionFactorDto.cs
@@ -95,86 +95,6 @@ namespace Hymson.MES.Services.Dtos.Process
         public List<ProcLoadPointLinkResourceDto>? LinkResources { get; set; }
     }
 
-    public record ProcConversionFactorCreateDto : BaseEntityDto
-    {
-        /// <summary>
-        /// 工序BOM代码
-        /// </summary>
-        public string Code { get; set; }
-
-        /// <summary>
-        /// 工序BOM名称
-        /// </summary>
-        public string Name { get; set; }
-
-
-        /// <summary>
-        /// 类型
-        /// </summary>
-        public ProcedureTypeEnum Type { get; set; }
-
-        /// <summary>
-        /// 包装等级（字典数据）
-        /// </summary>
-        public int? PackingLevel { get; set; }
-
-        /// <summary>
-        /// 所属资源类型ID
-        /// </summary>
-        public long? ResourceTypeId { get; set; }
-
-        /// <summary>
-        /// 循环次数
-        /// </summary>
-        public int? Cycle { get; set; }
-
-        /// <summary>
-        /// 是否维修返回
-        /// </summary>
-        public bool? IsRepairReturn { get; set; }
-
-        /// <summary>
-        /// 版本
-        /// </summary>
-        public string Version { get; set; } = "";
-
-        /// <summary>
-        /// 说明
-        /// </summary>
-        public string Remark { get; set; } = "";
-
-        /// <summary>
-        /// 是否复判
-        /// </summary>
-        public bool IsRejudges { get; set; }
-
-        /// <summary>
-        /// 设备NG信息校验
-        /// </summary>
-        public bool IsValidNGCodes { get; set; }
-
-        /// <summary>
-        /// 标记不合格id
-        /// </summary>
-        public long? MarkId { get; set; }
-
-        /// <summary>
-        /// 缺陷不合格ID
-        /// </summary>
-        public long? DefectId { get; set; }
-
-        /// <summary>
-        /// 是否复判
-        /// </summary>
-        public TrueOrFalseEnum? IsRejudge { get; set; }
-
-        /// <summary>
-        /// 是否校验NG信息
-        /// </summary>
-        public TrueOrFalseEnum? IsValidNGCode { get; set; }
-
-    }
-
     /// <summary>
     /// 转换系数表分页查询Dto
     /// </summary>
@@ -239,83 +159,6 @@ namespace Hymson.MES.Services.Dtos.Process
         public ManuSfcRepairDetailIsIsCloseEnum OpenStatus { get; set; }
     }
 
-    public class QueryConversionFactorDto 
-    {
-        /// <summary>
-        /// 描述 :转换系数
-        /// 空值 : false  
-        /// </summary>
-        public string conversionFactor { get; set; }
-
-
-        /// <summary>
-        /// 描述 :工序ID
-        /// 空值 : false  
-        /// </summary>
-        public long procedureId { get; set; }
-
-        /// <summary>
-        /// 工序编码
-        /// 空值 : false  
-        /// </summary>
-        public string code { get; set; }
-
-        /// <summary>
-        /// 工序名
-        /// 空值 : false  
-        /// </summary>
-        public string name { get; set; }
-
-
-        /// <summary>
-        /// 物料名
-        /// 空值 : false  
-        /// </summary>
-        public string MaterialName { get; set; }
-
-        /// <summary>
-        /// 物料编码
-        /// 空值 : false  
-        /// </summary>
-        public string MaterialCode { get; set; }
-
-        /// <summary>
-        /// 转换系数状态
-        /// </summary>
-        public ManuSfcRepairDetailIsIsCloseEnum OpenStatus { get; set; }
-        public List<ProcLoadPointLinkMaterialDto> LinkMaterials { get; set; }
-
-        public List<ProcLoadPointLinkResourceDto>? LinkResources { get; set; }
-
-    }
-
-    /// <summary>
-    /// 转换系数表新增Dto
-    /// </summary>
-    public record AddProcConversionFactorDto : BaseEntityDto
-    {
-        /// <summary>
-        /// 工序信息
-        /// </summary>
-        public ProcProcedureCreateDto Procedure { get; set; }
-
-        /// <summary>
-        /// 工序打印配置信息
-        /// </summary>
-
-        public List<ProcProcedurePrintReleationCreateDto> ProcedurePrintList { get; set; }
-
-        /// <summary>
-        /// 工序工作配置信息
-        /// </summary>
-        public List<InteJobBusinessRelationCreateDto> ProcedureJobList { get; set; }
-
-        /// <summary>
-        ///产出设置信息
-        /// </summary>
-        public List<ProcProductSetCreateDto> ProductSetList { get; set; }
-    }
-
     /// <summary>
     /// 转换系数表新增Dto
     /// </summary>
@@ -346,6 +189,11 @@ namespace Hymson.MES.Services.Dtos.Process
         /// </summary>
         public string name { get; set; }
 
+        /// <summary>
+        /// 工序名
+        /// 空值 : false  
+        /// </summary>
+        public string remark { get; set; }
 
         /// <summary>
         /// 转换系数状态
@@ -355,33 +203,6 @@ namespace Hymson.MES.Services.Dtos.Process
 
         public List<ProcLoadPointLinkResourceDto>? LinkResources { get; set; }
 
-    }
-
-    /// <summary>
-    /// 转换系数表修改Dto
-    /// </summary>
-    public record UpdateProcConversionFactorDto : BaseEntityDto
-    {
-        /// <summary>
-        /// 工序信息
-        /// </summary>
-        public ProcProcedureModifyDto Procedure { get; set; }
-
-        /// <summary>
-        /// 工序打印配置信息
-        /// </summary>
-
-        public List<ProcProcedurePrintReleationCreateDto> ProcedurePrintList { get; set; }
-
-        /// <summary>
-        /// 工序工作配置信息
-        /// </summary>
-        public List<InteJobBusinessRelationCreateDto> ProcedureJobList { get; set; }
-
-        /// <summary>
-        ///产出设置信息
-        /// </summary>
-        public List<ProcProductSetCreateDto> ProductSetList { get; set; }
     }
 
     /// <summary>
@@ -433,69 +254,5 @@ namespace Hymson.MES.Services.Dtos.Process
         public List<ProcLoadPointLinkMaterialDto> LinkMaterials { get; set; }
 
         public List<ProcLoadPointLinkResourceDto>? LinkResources { get; set; }
-    }
-
-    /// <summary>
-    /// 工序配置打印查询实体类
-    /// </summary>
-    public class ConversionFactorJobReleationDto
-    {
-        /// <summary>
-        /// 关联点
-        /// </summary>
-        public int LinkPoint { get; set; } = -1;
-
-        /// <summary>
-        /// 所属不合格代码ID
-        /// </summary>
-        public long BusinessId { get; set; }
-
-        /// <summary>
-        /// 作业ID
-        /// </summary>
-        public long JobId { get; set; }
-
-        /// <summary>
-        /// 是否启用
-        /// </summary>
-        public bool IsUse { get; set; }
-
-        /// <summary>
-        /// 参数
-        /// </summary>
-        public string Parameter { get; set; }
-
-        /// <summary>
-        /// 作业编号 
-        /// </summary>
-        public string Code { get; set; }
-
-        /// <summary>
-        /// 作业名称 
-        /// </summary>
-        public string Name { get; set; }
-
-        /// <summary>
-        /// 作业描述
-        /// </summary>
-        public string Remark { get; set; } = "";
-    }
-
-    public class ProcConversionFactorCodeDto
-    {
-        /// <summary>
-        /// 主键id
-        /// </summary>
-        public long Id { get; set; }
-
-        /// <summary>
-        /// 工序BOM代码
-        /// </summary>
-        public string Code { get; set; }
-
-        /// <summary>
-        /// 工序BOM名称
-        /// </summary>
-        public string Name { get; set; }
     }
 }
