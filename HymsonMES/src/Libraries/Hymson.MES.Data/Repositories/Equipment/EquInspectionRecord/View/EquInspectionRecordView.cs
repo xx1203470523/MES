402c0eadbee380676cb4ac16e23ea732c1715f1d
赵青 <zhao.qing@hymson.com>
2024-04-17 06:12:39 +0000

    feat: 设备点检

---
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Equipment/EquInspectionRecord/View/EquInspectionRecordView.cs
@@ -0,0 +1,71 @@
+﻿using Hymson.Infrastructure;
+using Hymson.MES.Core.Domain.Equipment;
+using Hymson.MES.Core.Enums;
+
+namespace Hymson.MES.Data.Repositories.Equipment
+{
+    public class EquInspectionRecordView : BaseEntity
+    {
+        public long Id { get; set; }
+
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        public DateTime? StartExecuTime { get; set; }
+
+        /// <summary>
+        /// 点检单号
+        /// </summary>
+        public string OrderCode { get; set; }
+
+        /// <summary>
+        /// 点检任务Id
+        /// </summary>
+        public long InspectionTaskSnapshootId { get; set; }
+
+        /// <summary>
+        /// 工作中心Id
+        /// </summary>
+        public long WorkCenterId { get; set; }
+
+        /// <summary>
+        /// 工作中心编码
+        /// </summary>
+        public string WorkCenterCode { get; set; }
+
+        /// <summary>
+        /// 设备编码
+        /// </summary>
+        public string EquipmentCode { get; set; }
+
+        /// <summary>
+        /// 设备名称
+        /// </summary>
+        public string EquipmentName { get; set; }
+
+        /// <summary>
+        /// 点检类型 1、日点检 2、周点检
+        /// </summary>
+        public EquInspectionTypeEnum InspectionType { get; set; }
+
+        /// <summary>
+        /// 类别 1、白班2、晚班3、巡检
+        /// </summary>
+        public EquInspectionTaskTypeEnum? Type { get; set; }
+
+        /// <summary>
+        /// 完成时长（分钟）
+        /// </summary>
+        public int? CompleteTime { get; set; }
+
+        /// <summary>
+        /// 是否合格
+        /// </summary>
+        public bool? IsQualified { get; set; }
+
+        /// <summary>
+        /// 1、待检验2、检验中3、已完成
+        /// </summary>
+        public EquInspectionRecordStatusEnum Status { get; set; }
+    }
+}
---
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Services/Equipment/EquInspectionItem/IEquInspectionItemService.cs
@@ -0,0 +1,54 @@
+using Hymson.Infrastructure;
+using Hymson.MES.Services.Dtos.Equipment;
+
+namespace Hymson.MES.Services.Services.Equipment
+{
+    /// <summary>
+    /// 服务接口（设备点检保养项目）
+    /// </summary>
+    public interface IEquInspectionItemService
+    {
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="saveDto"></param>
+        /// <returns></returns>
+        Task<int> CreateAsync(EquInspectionItemSaveDto saveDto);
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="saveDto"></param>
+        /// <returns></returns>
+        Task<int> ModifyAsync(EquInspectionItemSaveDto saveDto);
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<int> DeleteAsync(long id);
+
+        /// <summary>
+        /// 批量删除
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        Task<int> DeletesAsync(long[] ids);
+
+        /// <summary>
+        /// 根据ID查询
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<EquInspectionItemDto?> QueryByIdAsync(long id);
+
+        /// <summary>
+        /// 获取分页List
+        /// </summary>
+        /// <param name="pagedQueryDto"></param>
+        /// <returns></returns>
+        Task<PagedInfo<EquInspectionItemDto>> GetPagedListAsync(EquInspectionItemPagedQueryDto pagedQueryDto);
+
+    }
+}
