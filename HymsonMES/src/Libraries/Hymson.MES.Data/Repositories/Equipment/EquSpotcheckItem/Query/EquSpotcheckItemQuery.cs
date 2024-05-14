cf39019e68f9b75cfddfd44421bd9c35c30b7c2d
Jam <xpow@qq.com>
2024-05-14 19:02:59 +0800

    fix设备点检项目修改

--- HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Equipment/EquSpotcheckItem/Query/EquSpotcheckItemQuery.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Equipment/EquSpotcheckItem/Query/EquSpotcheckItemQuery.cs
@@ -5,6 +5,10 @@ namespace Hymson.MES.Data.Repositories.Equipment.Query
     /// </summary>
     public class EquSpotcheckItemQuery
     {
-    
+        /// <summary>
+        /// 站点Id
+        /// </summary>
+        public long? SiteId { get; set; }
+        public string? Code { get; set; }   
     }
 }
