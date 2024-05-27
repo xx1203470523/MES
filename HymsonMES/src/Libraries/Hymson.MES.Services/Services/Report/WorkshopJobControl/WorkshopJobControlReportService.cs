da9fd253611aaadecffd42226c9dc44d472a0885
wangkeming <wangkeming998@163.com>
2024-05-27 11:37:31 +0800

    fix: 修复5009 【作业详情】 作业详情界面，操作时间的值错误，取成了操作当前作业详情的时间

--- HymsonMES/src/Libraries/Hymson.MES.Services/Services/Report/WorkshopJobControl/WorkshopJobControlReportService.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Services/Report/WorkshopJobControl/WorkshopJobControlReportService.cs
@@ -453,7 +453,9 @@ namespace Hymson.MES.Services.Services.Report
                 };
             }
             await StepAssignment(stepDetailDto.AfterStepDDto);
-            var manuSfcStepTypeobOrAssemblys = GetManuSfcStepTypeobOrAssemblys();
+            stepDetailDto.AfterStepDDto.CreatedBy = beforeStepEntity.CreatedBy;
+            stepDetailDto.AfterStepDDto.CreatedOn = beforeStepEntity.CreatedOn;
+           var manuSfcStepTypeobOrAssemblys = GetManuSfcStepTypeobOrAssemblys();
             var manuSfcStepTypeobOrAssembly = manuSfcStepTypeobOrAssemblys.FirstOrDefault(x => x.Key == beforeStepEntity.Operatetype);
             if (manuSfcStepTypeobOrAssembly != null)
             {
