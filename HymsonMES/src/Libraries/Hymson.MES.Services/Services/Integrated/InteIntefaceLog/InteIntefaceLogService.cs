9484489df7a8e0ab8de1e2cbe37520283090b50c
Jam <xpow@qq.com>
2024-05-29 20:44:56 +0800

    日志查询优化

--- HymsonMES/src/Libraries/Hymson.MES.Services/Services/Integrated/InteIntefaceLog/InteIntefaceLogService.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Services/Integrated/InteIntefaceLog/InteIntefaceLogService.cs
@@ -23,11 +23,11 @@ namespace Hymson.MES.Services.Services.Integrated.InteIntefaceLog
         }
 
         /// <summary>
-        /// 根据查询条件获取分页数据
+        /// 根据查询条件获取分页数据 InteIntefaceLogDto
         /// </summary>
         /// <param name="pagedQueryDto"></param>
         /// <returns></returns>
-        public async Task<PagedInfo<InteIntefaceLogDto>> GetPagedListAsync(InteIntefaceLogPagedQueryDto pagedQueryDto)
+        public async Task<PagedInfo<TraceLogEntry>> GetPagedListAsync(InteIntefaceLogPagedQueryDto pagedQueryDto)
         {
             var logDataPagedQuery = new LogDataPagedQuery()
             {
@@ -67,86 +67,28 @@ namespace Hymson.MES.Services.Services.Integrated.InteIntefaceLog
                 logDataPagedQuery.InterfaceCode = pagedQueryDto.InterfaceCode;
             }
 
-            if (pagedQueryDto?.InterfaceName != null)
-            {
-                logDataPagedQuery.Message = pagedQueryDto.InterfaceName;
-            }
 
             var data = new Dictionary<string, string> { };
 
-            if (pagedQueryDto?.Requestor != null)
+            if (pagedQueryDto?.Data?.Code != null)
             {
-                data.Add("Name", pagedQueryDto.Requestor);
+                data.Add("Code", pagedQueryDto.Data.Code);
             }
 
-            if (pagedQueryDto?.Responsetor != null)
+            if (pagedQueryDto?.Data?.ReceiverType != null)
             {
-                data.Add("ReceiverType", pagedQueryDto.Responsetor);
+                data.Add("ReceiverType", pagedQueryDto.Data.ReceiverType);
             }
 
-            if (pagedQueryDto?.ResponseResult != null)
+            if (pagedQueryDto?.Data?.IsSuccess != null)
             {
-                var isSuccess = (int)pagedQueryDto.ResponseResult;
-                data.Add("IsSuccess", isSuccess.ToString() ?? "0");
+                data.Add("IsSuccess", pagedQueryDto.Data.IsSuccess.ToString() ?? "0");
             }
 
             logDataPagedQuery.Data = data;
 
-            var getlogdate = await _logDataService.GetLogDataPagedAsync(logDataPagedQuery);
-
-            List<InteIntefaceLogDto> dtos = new();
-
-            foreach (var item in getlogdate.Data)
-            {
-                dtos.Add(MapToDto(item));
-            }
-
-            return new PagedInfo<InteIntefaceLogDto>(dtos, getlogdate.PageIndex, getlogdate.PageSize, getlogdate.TotalCount);
+            return await _logDataService.GetLogDataPagedAsync(logDataPagedQuery);
         }
 
-        private InteIntefaceLogDto MapToDto(TraceLogEntry entry)
-        {
-            var dto = new InteIntefaceLogDto
-            {
-                Id = entry.Id,
-                InterfaceCode = entry.InterfaceCode ?? entry.Data["Path"],
-                InterfaceName = entry.Message,
-                Requestor = entry.Data.ContainsKey("Name") ? entry.Data["Name"] : string.Empty,
-                Responsetor = entry.Data.ContainsKey("ReceiverType") ? entry.Data["ReceiverType"] : string.Empty,
-                RequestTime = entry.Data.ContainsKey("RequestTime") ? ConverToIncludeSeconds(entry.Data["RequestTime"]) : string.Empty,
-                ResponseTime = entry.Data.ContainsKey("ResponseTime") ? ConverToIncludeSeconds(entry.Data["ResponseTime"]) : string.Empty,
-                Cost = entry.Data.ContainsKey("Cost") ? entry.Data["Cost"] : string.Empty,
-                Ip = entry.Data.ContainsKey("Ip") ? entry.Data["Ip"] : string.Empty,
-                Method = entry.Data["Method"],
-                RequestBody = entry.Data["RequestBody"],
-                ResponseBody = entry.Data["ResponseBody"],
-                Path = entry.Data["Path"]
-            };
-
-            if (entry.Data.ContainsKey("IsSuccess") && int.TryParse(entry.Data["IsSuccess"], out int isSuccessResult))
-            {
-                dto.ResponseResult = (ResponseResultEnum)isSuccessResult;
-            }
-
-            return dto;
-        }
-
-        /// <summary>
-        /// 转成包含到毫秒的时间
-        /// </summary>
-        /// <param name="_time"></param>
-        /// <returns></returns>
-        private string ConverToIncludeSeconds(string _time)
-        {
-            string formattedDateTime = _time;
-            if (!string.IsNullOrEmpty(_time))
-            {
-                if (DateTime.TryParse(_time, out DateTime requestDateTime))
-                {
-                    formattedDateTime = requestDateTime.ToString("MM-dd HH:mm:ss.fff");
-                }
-            }
-            return formattedDateTime;
-        }
     }
 }
