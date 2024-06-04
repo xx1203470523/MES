a898139da81e54ee62ca5c7d27810fb45ecb41be
王克明 <wang.keming@if-smart.com>
2024-06-04 05:42:49 +0000

    Dev

---
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Report/ManuDowngradingDetailReport/Query/ManuDowngradingDetailReportQuery.cs
@@ -0,0 +1,10 @@
+namespace Hymson.MES.Data.Repositories.Report.Query
+{
+    /// <summary>
+    /// 降级品明细报表 查询参数
+    /// </summary>
+    public class ManuDowngradingDetailReportQuery
+    {
+    
+    }
+}
---
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Dtos/Report/VehicleFreightRecordDto.cs
@@ -0,0 +1,120 @@
+﻿using Hymson.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymson.MES.Services.Dtos.Report
+{
+    public class VehicleFreightRecordDto
+    {
+        /// <summary>
+        /// 工作中心
+        /// </summary>
+        public string WorkCenterCode { get; set; }
+
+        /// <summary>
+        /// 工序编码
+        /// </summary>
+        public string ProcedureCode { get; set; }
+
+        /// <summary>
+        /// 工序名称
+        /// </summary>
+        public string ProcedureName { get; set; }
+
+        /// <summary>
+        /// 工单号
+        /// </summary>
+        public string OrderCode { get; set; }
+
+        /// <summary>
+        /// 资源编码
+        /// </summary>
+        public string ResourceCode { get; set; }
+
+        /// <summary>
+        /// 设备编码
+        /// </summary>
+        public string EquipmentCode { get; set; }
+
+        /// <summary>
+        /// 条码
+        /// </summary>
+        public string BarCode { get; set; }
+
+        /// <summary>
+        /// 托盘编码
+        /// </summary>
+        public string VehicleCode { get; set; }
+
+        /// <summary>
+        /// 绑定类型
+        /// </summary>
+        public int OperateType { get; set; }
+
+        /// <summary>
+        /// 解绑时间
+        /// </summary>
+        public string BindAndUnbindTime { get; set; }
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public string CreateBy { get; set; }
+
+        /// <summary>
+        /// 位置号
+        /// </summary>
+        public string Location { get; set; }
+    }
+
+    public class VehicleFreightRecordQueryDto : PagerInfo
+    {
+        /// <summary>
+        /// 站点id
+        /// </summary>
+        public long? SiteId { get; set; }
+
+        /// <summary>
+        /// 条码
+        /// </summary>
+        public string? BarCode { get; set; }
+
+        /// <summary>
+        /// 工作中心Id
+        /// </summary>
+        public long? WorkCenterId { get; set; }
+
+        /// <summary>
+        /// 设备编码
+        /// </summary>
+        public long? EquipmentId { get; set; }
+
+        /// <summary>
+        /// 工单ID
+        /// </summary>
+        public long? WorkOrderId { get; set; }
+
+        /// <summary>
+        /// 工序Id
+        /// </summary>
+        public long? ProcedureId { get; set; }
+
+        /// <summary>
+        /// 托盘Id
+        /// </summary>
+        public long? VehicleId { get; set; }
+
+        /// <summary>
+        /// 资源Id
+        /// </summary>
+        public long? ResourceId { get; set; }
+
+        /// <summary>
+        /// 计划开始时间  数组 ：时间范围 
+        /// </summary>
+        public DateTime[]? CreatedOn { get; set; }
+    }
+}
