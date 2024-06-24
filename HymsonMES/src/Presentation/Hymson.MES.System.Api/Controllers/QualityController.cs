6a700d0c1d96a20473f7808f2db6e776e7a0bfc5
Czhipu <czhipu@126.com>
2024-06-24 13:40:05 +0800

    feat:  添加来料检验接口

---
+++ HymsonMES/src/Presentation/Hymson.MES.System.Api/Controllers/QualityController.cs
@@ -0,0 +1,53 @@
+﻿using Hymson.MES.SystemServices.Dtos;
+using Hymson.MES.SystemServices.Services.Quality;
+using Hymson.Web.Framework.Attributes;
+using Hymson.Web.Framework.Filters.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hymson.MES.System.Api.Controllers
+{
+    /// <summary>
+    /// 质量
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class QualityController : ControllerBase
+    {
+        /// <summary>
+        /// 日志
+        /// </summary>
+        private readonly ILogger<QualityController> _logger;
+
+        /// <summary>
+        /// 服务接口（来料检验）
+        /// </summary>
+        private readonly IQualIQCService _qualIQCService;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="qualIQCService"></param>
+        public QualityController(ILogger<QualityController> logger, IQualIQCService qualIQCService)
+        {
+            _logger = logger;
+            _qualIQCService = qualIQCService;
+        }
+
+        /// <summary>
+        /// 提交（来料检验）
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPost("iqc")]
+        [ProducesResponseType(typeof(ResultDto), 200)]
+        [LogDescription("来料检验", BusinessType.INSERT)]
+        public async Task SubmitIncomingAsync([FromBody] IQCRequestDto dto)
+        {
+            await _qualIQCService.SubmitIncomingAsync(dto);
+        }
+
+    }
+}
