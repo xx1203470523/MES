16e51543e393da39eabafca9833591b5765c433c
zhao.qing <cindy_zhao@xnebula.com>
2024-07-03 10:45:27 +0800

    feat: job维护关联规则，库存接收全部允许更改库存

---
+++ HymsonMES/src/Libraries/Hymson.MES.Data/Repositories/Integrated/InteJobConfig/IInteJobConfigRepository.cs
@@ -0,0 +1,91 @@
+using Hymson.Infrastructure;
+using Hymson.MES.Core.Domain.Integrated;
+using Hymson.MES.Data.Repositories.Common.Command;
+using Hymson.MES.Data.Repositories.Integrated.Query;
+
+namespace Hymson.MES.Data.Repositories.Integrated
+{
+    /// <summary>
+    /// 仓储接口（作业配置）
+    /// </summary>
+    public interface IInteJobConfigRepository
+    {
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<int> InsertAsync(InteJobConfigEntity entity);
+
+        /// <summary>
+        /// 新增（批量）
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        Task<int> InsertRangeAsync(IEnumerable<InteJobConfigEntity> entities);
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<int> UpdateAsync(InteJobConfigEntity entity);
+
+        /// <summary>
+        /// 更新（批量）
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        Task<int> UpdateRangeAsync(IEnumerable<InteJobConfigEntity> entities);
+
+        /// <summary>
+        /// 软删除  
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<int> DeleteAsync(long id);
+
+        /// <summary>
+        /// 软删除（批量）
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        Task<int> DeletesAsync(DeleteCommand command);
+
+        /// <summary>
+        /// 删除（物理删除）
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<int> DeleteByJobIdAsync(long id);
+
+        /// <summary>
+        /// 根据ID获取数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<InteJobConfigEntity> GetByIdAsync(long id);
+
+        /// <summary>
+        /// 根据IDs获取数据（批量）
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        Task<IEnumerable<InteJobConfigEntity>> GetByIdsAsync(long[] ids);
+
+        /// <summary>
+        /// 获取List
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        Task<IEnumerable<InteJobConfigEntity>> GetEntitiesAsync(InteJobConfigQuery query);
+
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <param name="pagedQuery"></param>
+        /// <returns></returns>
+        Task<PagedInfo<InteJobConfigEntity>> GetPagedListAsync(InteJobConfigPagedQuery pagedQuery);
+
+    }
+}
