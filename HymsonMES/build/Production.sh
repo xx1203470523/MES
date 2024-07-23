3166556d33cbc6689bc95c0be25d6164888794c0
JinYi <584507257@qq.com>
2024-07-23 18:26:12 +0800

    feat:脚本加执行权限

--- HymsonMES/build/Production.sh
+++ HymsonMES/build/Production.sh
@@ -4,6 +4,6 @@ timestamp=$(date +%Y%m%d%H%M%S)
 serviceName=hymson-mes-api-prod
 registryUrl=harbor.xnebula.com/new-energy
 imageName=hymson.mes.api
-docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.Api/Dockerfile .
-docker tag $imageName:$timestamp  $registryUrl/$imageName:$timestamp
-docker push $registryUrl/$imageName:$timestamp
+sudo docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.Api/Dockerfile .
+sudo docker tag $imageName:$timestamp  $registryUrl/$imageName:$timestamp
+sudo docker push $registryUrl/$imageName:$timestamp
