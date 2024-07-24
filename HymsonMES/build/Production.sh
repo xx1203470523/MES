5d4acf7d9ddf11f851c7db3d8b4c9e8f1480fbed
JinYi <584507257@qq.com>
2024-07-24 16:11:04 +0800

    feat:修改构建脚本

--- HymsonMES/build/Production.sh
+++ HymsonMES/build/Production.sh
@@ -8,18 +8,18 @@ sudo docker tag $imageName:$timestamp  $registryUrl/$imageName:$timestamp
 sudo docker push $registryUrl/$imageName:$timestamp
 
 imageName=hymson.mes.equipment.api
-sudo docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.Equipment.Api/sudo dockerfile .
+sudo docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.Equipment.Api/Dockerfile .
 sudo docker tag $imageName:$timestamp  $registryUrl/$imageName:$timestamp
 sudo docker push $registryUrl/$imageName:$timestamp
 
 
 imageName=hymson.mes.backgroundtasks
-sudo docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.BackgroundTasks/sudo dockerfile .
+sudo docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.BackgroundTasks/Dockerfile .
 sudo docker tag $imageName:$timestamp  $registryUrl/$imageName:$timestamp
 sudo docker push $registryUrl/$imageName:$timestamp
 
 
 imageName=hymson.mes.system.api
-sudo docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.System.Api/sudo dockerfile .
+sudo docker build  --build-arg PublishEnvironment=$PublishEnvironment  -t $imageName:$timestamp -f ./HymsonMES/src/Presentation/Hymson.MES.System.Api/Dockerfile .
 sudo docker tag $imageName:$timestamp  $registryUrl/$imageName:$timestamp
 sudo docker push $registryUrl/$imageName:$timestamp
