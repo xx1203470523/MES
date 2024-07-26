f17db8074483db42bf0ca215c6c4b9417670972a
yuan.xixi <szxx809@20240201>
2024-07-26 19:26:40 +0800

    feat: NIO推送优化 1. NIO推送优化 2. LMS汇总添加NG记录

---
+++ HymsonMES/src/Libraries/Hymson.MES.BackgroundServices.NIO/Dtos/Buz/SfcBatchDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hymson.MES.BackgroundServices.NIO.Dtos.Buz
+{
+    /// <summary>
+    /// 条码批次信息
+    /// </summary>
+    public class SfcBatchDto
+    {
+        /// <summary>
+        /// 条码
+        /// </summary>
+        public string Sfc { get; set; }
+
+        /// <summary>
+        /// 批次码
+        /// </summary>
+        public string Batch { get; set; }
+    }
+}
