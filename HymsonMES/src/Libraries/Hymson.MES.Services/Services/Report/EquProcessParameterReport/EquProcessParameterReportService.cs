aa4bd2df5bc63713c697a58905321c91d0c4120b
zhao.qing <cindy_zhao@xnebula.com>
2024-07-29 18:06:20 +0800

    feat: 设备过程参数增加导出功能

--- HymsonMES/src/Libraries/Hymson.MES.Services/Services/Report/EquProcessParameterReport/EquProcessParameterReportService.cs
+++ HymsonMES/src/Libraries/Hymson.MES.Services/Services/Report/EquProcessParameterReport/EquProcessParameterReportService.cs
@@ -3,18 +3,23 @@ using Elastic.Clients.Elasticsearch.Core.Search;
 using Hymson.Authentication;
 using Hymson.Authentication.JwtBearer;
 using Hymson.Authentication.JwtBearer.Security;
+using Hymson.Excel.Abstractions;
 using Hymson.Infrastructure;
 using Hymson.Infrastructure.Exceptions;
 using Hymson.Infrastructure.Mapper;
+using Hymson.Localization.Services;
 using Hymson.MES.Core.Constants;
 using Hymson.MES.Core.Domain.Integrated;
 using Hymson.MES.Core.Domain.Process;
+using Hymson.MES.Core.Enums;
 using Hymson.MES.Data.Repositories.Equipment.EquEquipment;
 using Hymson.MES.Data.Repositories.Integrated.IIntegratedRepository;
 using Hymson.MES.Data.Repositories.Parameter;
 using Hymson.MES.Data.Repositories.Process;
+using Hymson.MES.Services.Dtos.Process;
 using Hymson.MES.Services.Dtos.Report;
 using Hymson.MES.Services.Services.Equipment.EquEquipment;
+using Hymson.Minio;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
@@ -55,6 +60,10 @@ namespace Hymson.MES.Services.Services.Report
         private readonly IProcResourceRepository _resourceRepository;
         private readonly IProcResourceEquipmentBindRepository _equipmentBindRepository;
 
+        private readonly ILocalizationService _localizationService;
+        private readonly IExcelService _excelService;
+        private readonly IMinioService _minioService;
+
         /// <summary>
         /// 构造函数
         /// </summary>
@@ -65,7 +74,10 @@ namespace Hymson.MES.Services.Services.Report
                IEquEquipmentRepository equipmentRepository,
                IProcParameterRepository parameterRepository,
                IProcResourceRepository resourceRepository,
-               IProcResourceEquipmentBindRepository equipmentBindRepository)
+               IProcResourceEquipmentBindRepository equipmentBindRepository,
+               ILocalizationService localizationService,
+               IExcelService excelService,
+               IMinioService minioService)
         {
             _currentUser = currentUser;
             _currentSite = currentSite;
@@ -76,6 +88,10 @@ namespace Hymson.MES.Services.Services.Report
             _parameterRepository = parameterRepository;
             _resourceRepository = resourceRepository;
             _equipmentBindRepository = equipmentBindRepository;
+
+            _localizationService = localizationService;
+            _excelService = excelService;
+            _minioService = minioService;
         }
 
         /// <summary>
@@ -171,6 +187,117 @@ namespace Hymson.MES.Services.Services.Report
                 listDto.Add(equParameterDto);
             }
             return new PagedInfo<EquProcessParameterReportDto>(listDto, pagedInfo.PageIndex, pagedInfo.PageSize, pagedInfo.TotalCount);
+        }
+
+        /// <summary>
+        /// 根据查询条件导出物料数据
+        /// </summary>
+        /// <param name="pagedQueryDto"></param>
+        /// <returns></returns>
+        public async Task<EquProcessParameterExportResultDto> ExprotEquProcessParameterListAsync(EquProcessParameterReportPagedQueryDto pagedQueryDto)
+        {
+            //选择时间与设备编号显示数据
+            if (pagedQueryDto.CreatedOn == null || pagedQueryDto.CreatedOn.Length < 2)
+            {
+                throw new CustomerValidationException(nameof(ErrorCode.MES14953));
+            }
+            if (pagedQueryDto.EquipmentId <= 0)
+            {
+                throw new CustomerValidationException(nameof(ErrorCode.MES12601));
+            }
+
+            var siteId = _currentSite.SiteId ?? 0;
+            //获取设备的参数信息
+            var parameterQuery = new ManuEquipmentParameterPagedQuery
+            {
+                SiteId = siteId,
+                CreatedOn = pagedQueryDto.CreatedOn,
+                ParameterId = pagedQueryDto.ParameterId,
+                EquipmentId = pagedQueryDto.EquipmentId,
+                PageIndex = pagedQueryDto.PageIndex,
+                PageSize = 1000
+            };
+            var pagedInfo = await _equipmentParameterRepository.GetParametesByEqumentIdEntitiesAsync(parameterQuery);
+
+            List<EquProcessParameterExportDto> listDto = new List<EquProcessParameterExportDto>();
+            if (pagedInfo.Data == null || !pagedInfo.Data.Any())
+            {
+                var filePathN = await _excelService.ExportAsync(listDto, _localizationService.GetResource("EquProcessParameter"), _localizationService.GetResource("EquProcessParameter"));
+                //上传到文件服务器
+                var uploadResultN = await _minioService.PutObjectAsync(filePathN);
+                return new EquProcessParameterExportResultDto
+                {
+                    FileName = _localizationService.GetResource("EquProcessParameter"),
+                    Path = uploadResultN.AbsoluteUrl,
+                };
+            }
+
+            var equipmentId = pagedQueryDto.EquipmentId;
+            var parameterIds = pagedInfo.Data.Select(x => x.ParameterId).Distinct().ToList();
+
+            //设备
+            var equipmentEntity = await _equipmentRepository.GetByIdAsync(equipmentId);
+
+            //参数
+            var parameterEntities = await _parameterRepository.GetByIdsAsync(parameterIds);
+
+            //资源
+            var resourceEntity = new ProcResourceEntity();
+            var workCenterEntity = new InteWorkCenterEntity();
+            IEnumerable<ProcProcedureEntity> procedureEntities = new List<ProcProcedureEntity>();
+            var equipmentBindEntities = await _equipmentBindRepository.GetByResourceIdAsync(new ProcResourceEquipmentBindQuery
+            {
+                Ids = new long[] { equipmentId }
+            });
+            var resourceIds = equipmentBindEntities.Select(x => x.ResourceId).Distinct().ToList();
+            if (resourceIds.Any())
+            {
+                var resourceId = resourceIds[0];
+                resourceEntity = await _resourceRepository.GetResByIdAsync(resourceId);
+                if (resourceEntity != null)
+                {
+                    //工作中心
+                    workCenterEntity = await _inteWorkCenterRepository.GetByResourceIdAsync(resourceId);
+
+                    //工序
+                    procedureEntities = await _procProcedureRepository.GetProcProcedureByResourceTypeIdAsync(resourceEntity.ResTypeId);
+                }
+            }
+
+            foreach (var item in pagedInfo.Data)
+            {
+                var resTypeId = resourceEntity?.ResTypeId ?? 0;
+                var procedureEntity = procedureEntities.FirstOrDefault(y => y.ResourceTypeId == resTypeId);
+                var parameterEntity = parameterEntities.FirstOrDefault(x => x.Id == item.ParameterId);
+                var equParameterDto = new EquProcessParameterExportDto
+                {
+                    WorkCenterName = workCenterEntity?.Name ?? "",
+                    ProcedureCode = procedureEntity?.Code ?? "",
+                    ProcedureName = procedureEntity?.Name ?? "",
+                    ResCode = resourceEntity?.ResCode ?? "",
+                    ResName = resourceEntity?.ResName ?? "",
+                    EquipmentCode = equipmentEntity?.EquipmentCode ?? "",
+                    EquipmentName = equipmentEntity?.EquipmentName ?? "",
+                    ParameterCode = parameterEntity?.ParameterCode ?? "",
+                    ParameterName = parameterEntity?.ParameterName ?? "",
+                    ParameterUnit = parameterEntity?.ParameterUnit ?? "",
+                    ParameterValue = item.ParameterValue,
+                    CollectionTime = item.CollectionTime,
+                    CreatedOn = item.CreatedOn
+                };
+
+                listDto.Add(equParameterDto);
+            }
+
+            var filePath = await _excelService.ExportAsync(listDto, _localizationService.GetResource("EquProcessParameter"), _localizationService.GetResource("EquProcessParameter"));
+            //上传到文件服务器
+            var uploadResult = await _minioService.PutObjectAsync(filePath);
+            return new EquProcessParameterExportResultDto
+            {
+                FileName = _localizationService.GetResource("EquProcessParameter"),
+                Path = uploadResult.AbsoluteUrl,
+            };
+
         }
 
     }
